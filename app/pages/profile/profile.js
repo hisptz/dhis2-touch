bb0c7b63086d15e7f7ec53fa8c9a89e206f8acf7
Joseph Chingalo <profschingalo@gmail.com>
2016-10-06 16:44:46 +0300

    updata loading view for all home pages of apps

--- app/pages/profile/profile.js
+++ app/pages/profile/profile.js
@@ -9,6 +9,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var ionic_angular_1 = require('ionic-angular');
+var objectToArray_1 = require('../../pipes/objectToArray');
+var user_1 = require('../../providers/user/user');
+var app_provider_1 = require('../../providers/app-provider/app-provider');
+var http_client_1 = require("../../providers/http-client/http-client");
+var sql_lite_1 = require("../../providers/sql-lite/sql-lite");
 /*
   Generated class for the ProfilePage page.
 
@@ -16,12 +21,32 @@ var ionic_angular_1 = require('ionic-angular');
   Ionic pages and navigation.
 */
 var ProfilePage = (function () {
-    function ProfilePage(navCtrl, toastCtrl) {
+    function ProfilePage(navCtrl, toastCtrl, user, appProvider, sqlLite, httpClient) {
         this.navCtrl = navCtrl;
         this.toastCtrl = toastCtrl;
+        this.user = user;
+        this.appProvider = appProvider;
+        this.sqlLite = sqlLite;
+        this.httpClient = httpClient;
         this.loadingData = false;
         this.loadingMessages = [];
+        this.loadingProfileInformation();
     }
+    ProfilePage.prototype.loadingProfileInformation = function () {
+        var _this = this;
+        this.loadingData = true;
+        this.loadingMessages = [];
+        this.setLoadingMessages('Loading profiles information');
+        this.user.getUserData().then(function (userData) {
+            _this.profileInformation = userData;
+            alert(JSON.stringify(userData));
+            alert(JSON.stringify(userData.userRoles));
+            alert(JSON.stringify(userData.organisationUnits));
+        }, function (error) {
+            _this.loadingData = false;
+            _this.setToasterMessage('Fail to load profile information');
+        });
+    };
     ProfilePage.prototype.setLoadingMessages = function (message) {
         this.loadingMessages.push(message);
     };
@@ -42,8 +67,10 @@ var ProfilePage = (function () {
     ProfilePage = __decorate([
         core_1.Component({
             templateUrl: 'build/pages/profile/profile.html',
+            providers: [user_1.User, app_provider_1.AppProvider, http_client_1.HttpClient, sql_lite_1.SqlLite],
+            pipes: [objectToArray_1.ObjectToArray]
         }), 
-        __metadata('design:paramtypes', [ionic_angular_1.NavController, ionic_angular_1.ToastController])
+        __metadata('design:paramtypes', [ionic_angular_1.NavController, ionic_angular_1.ToastController, user_1.User, app_provider_1.AppProvider, sql_lite_1.SqlLite, http_client_1.HttpClient])
     ], ProfilePage);
     return ProfilePage;
 })();
--- app/pages/profile/profile.ts
+++ app/pages/profile/profile.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController,ToastController } from 'ionic-angular';
 
+import {ObjectToArray} from '../../pipes/objectToArray';
+import {User} from '../../providers/user/user';
+import {AppProvider} from '../../providers/app-provider/app-provider';
+import {HttpClient} from "../../providers/http-client/http-client";
+import {SqlLite} from "../../providers/sql-lite/sql-lite";
+
 /*
   Generated class for the ProfilePage page.
 
@@ -9,15 +15,38 @@ import { NavController,ToastController } from 'ionic-angular';
 */
 @Component({
   templateUrl: 'build/pages/profile/profile.html',
+  providers : [User,AppProvider,HttpClient,SqlLite],
+  pipes : [ObjectToArray]
 })
 export class ProfilePage {
 
   private loadingData : boolean = false;
   private loadingMessages : any = [];
+  private profileInformation : any;
+  private userRoles : any;
+  private assignOrgUnits : any;
 
-  constructor(private navCtrl: NavController,private toastCtrl: ToastController) {
-
+  constructor(private navCtrl: NavController,private toastCtrl: ToastController,private user : User,private appProvider : AppProvider,private sqlLite : SqlLite,private httpClient: HttpClient) {
+    this.loadingProfileInformation();
   }
+
+  loadingProfileInformation(){
+    this.loadingData = true;
+    this.loadingMessages = [];
+    this.setLoadingMessages('Loading profiles information');
+    this.user.getUserData().then((userData :any)=>{
+      this.profileInformation = userData;
+      alert(JSON.stringify(userData));
+      alert(JSON.stringify(userData.userRoles));
+      alert(JSON.stringify(userData.organisationUnits));
+    },error=>{
+      this.loadingData = false;
+      this.setToasterMessage('Fail to load profile information');
+    });
+  }
+
+
+
 
   setLoadingMessages(message){
     this.loadingMessages.push(message);
