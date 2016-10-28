554b2c2e10bfd04acbd7de07115f337310a41253
Joseph Chingalo <profschingalo@gmail.com>
2016-10-28 11:08:39 +0300

    get user profile informations

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
+import {User} from '../../providers/user/user';
+import {AppProvider} from '../../providers/app-provider/app-provider';
+import {HttpClient} from "../../providers/http-client/http-client";
+import {SqlLite} from "../../providers/sql-lite/sql-lite";
 
 /*
   Generated class for the Profile page.
@@ -9,14 +14,86 @@ import { NavController } from 'ionic-angular';
 */
 @Component({
   selector: 'page-profile',
-  templateUrl: 'profile.html'
+  templateUrl: 'profile.html',
+  providers : [User,AppProvider,HttpClient,SqlLite]
 })
 export class Profile {
 
-  constructor(public navCtrl: NavController) {}
+  public loadingData : boolean = false;
+  public loadingMessages : any = [];
+  public currentUser : any;
+  public profileInformation : any;
+  public userRoles : any;
+  public assignedForms : any;
+  public assignedPrograms : any;
+  public assignOrgUnits : any;
+
+  constructor(public toastCtrl: ToastController,public user : User,public appProvider : AppProvider,public sqlLite : SqlLite,public httpClient: HttpClient) {
+    this.user.getCurrentUser().then(currentUser=>{
+      this.currentUser = currentUser;
+      this.loadingProfileInformation();
+    });
+  }
 
   ionViewDidLoad() {
-    console.log('Hello Profile Page');
+    //console.log('Hello Profile Page');
+  }
+
+  loadingProfileInformation(){
+    this.loadingData = true;
+    this.loadingMessages = [];
+
+
+    this.setLoadingMessages('Loading profiles information');
+    this.user.getUserData().then((userData :any)=>{
+      let data = {};
+      for(let key in userData){
+        let value = userData[key];
+        if(!(value instanceof Object)){
+          data[key] = value
+        }
+      }
+      this.profileInformation = this.getArrayFromObject(data);
+      alert(JSON.stringify(this.profileInformation));
+      //this.setUserRoles(userData);
+    },error=>{
+      this.loadingData = false;
+      this.setToasterMessage('Fail to load profile information');
+    });
+  }
+
+
+  getArrayFromObject(object){
+    let array = [];
+    for(let key in object){
+      let newValue = object[key];
+      if(newValue instanceof Object) {
+        newValue = JSON.stringify(newValue)
+      }
+      let newKey = (key.charAt(0).toUpperCase() + key.slice(1)).replace(/([A-Z])/g, ' $1').trim();
+      array.push({key : newKey,value : newValue})
+    }
+    return array;
+  }
+
+  setLoadingMessages(message){
+    this.loadingMessages.push(message);
+  }
+
+  setToasterMessage(message){
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  setStickToasterMessage(message){
+    let toast = this.toastCtrl.create({
+      message: message,
+      showCloseButton : true
+    });
+    toast.present();
   }
 
 }
