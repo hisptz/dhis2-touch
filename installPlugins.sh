94c2e0b8a0dbaaa678a28c657c45c41229b68094
Joseph Chingalo <profschingalo@gmail.com>
2016-10-28 10:16:15 +0300

    add script for update avilable plugins

---
+++ installPlugins.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+ionic plugin add cordova-plugin-app-version
+ionic plugin add cordova-plugin-console
+ionic plugin add cordova-plugin-device
+ionic plugin add cordova-plugin-splashscreen
+ionic plugin add cordova-plugin-statusbar
+ionic plugin add cordova-plugin-whitelist
+ionic plugin add cordova-sqlite-storage
+ionic plugin add ionic-plugin-keyboard
