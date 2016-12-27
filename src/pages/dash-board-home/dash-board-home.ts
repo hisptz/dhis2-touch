4890f3f2d0a2484d2bc3b9aed977b0ff5470d592
Joseph Chingalo <profschingalo@gmail.com>
2016-12-27 12:05:51 +0300

    list available dashbord as well as filter dashboard

--- src/pages/dash-board-home/dash-board-home.ts
+++ src/pages/dash-board-home/dash-board-home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,ToastController } from 'ionic-angular';
+import {HttpClient} from "../../providers/http-client/http-client";
+import {User} from "../../providers/user/user";
+import {Dashboard} from "../../providers/dashboard";
 
 /*
   Generated class for the DashBoardHome page.
@@ -9,44 +12,86 @@ import { NavController } from 'ionic-angular';
 */
 @Component({
   selector: 'page-dash-board-home',
-  templateUrl: 'dash-board-home.html'
+  templateUrl: 'dash-board-home.html',
+  providers : [User,HttpClient,Dashboard]
 })
 export class DashBoardHome {
 
   public options : any;
+  public chartType : string;
 
-  constructor(public navCtrl: NavController) {
+  public currentUser : any;
+  public loadingData : boolean = false;
+  public loadingMessages : any = [];
+  public dashBoards :any;
+  public dashBoardsCopy :any;
+
+  constructor(public navCtrl: NavController,public user : User,
+              public toastCtrl:ToastController,public dashboard : Dashboard,
+              public httpClient : HttpClient) {
+    this.drawChart();
+    this.user.getCurrentUser().then(user=>{
+      this.currentUser = user;
+      this.getAllDataBase();
+    });
+  }
+
+  getAllDataBase(){
+    this.loadingData = true;
+    this.loadingMessages = [];
+    this.setLoadingMessages("Loading available dashboards from server");
+    this.dashboard.getAllDashBoardsFromServer(this.currentUser).then((dashBoardResponse:any)=>{
+      this.loadingData = false;
+      this.dashBoards = dashBoardResponse.dashboards;
+      this.dashBoardsCopy = dashBoardResponse.dashboards;
+    },error=>{
+      this.loadingData = false;
+      this.dashBoards = [];
+      this.setToasterMessage("Fail to load dashboards from the server");
+    });
+  }
+
+  goToDashBoard(dashBoard){
+    alert(JSON.stringify(dashBoard));
+  }
+
+  getFilteredList(ev: any) {
+    let val = ev.target.value;
+    this.dashBoards = this.dashBoardsCopy;
+    if(val && val.trim() != ''){
+      this.dashBoards = this.dashBoards.filter((dashBoard:any) => {
+        return (dashBoard.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  changeType(type){
     this.options = {};
+    this.options = this.dashboard.getDefaultDashBoard(type);
+  }
+
+  changeChart(){
+    this.changeType(this.chartType);
   }
 
   drawChart(){
-    this.options = {
-      title: { text: 'simple chart' },
-      chart: { type: 'spline' },
-      series: [
-        {
-        data: [49.9, 17.5, 10.4]
-        },
-        {
-          data: [6.9, 11.5, 16.4]
-        },
-        {
-          data: [42.9, 30.5, 14.8]
-        },
-        {
-          data: [39.9, 18.5, 20.4]
-        },
-        {
-          data: [16.9, 15.5, 1.4]
-        },
-        {
-          data: [32.9, 3.5, 17.8]
-        }
-      ]
-    };
+    this.options = {};
+    this.options = this.dashboard.getDefaultDashBoard("");
   }
 
   ionViewDidLoad() {
+  }
+
+  setLoadingMessages(message){
+    this.loadingMessages.push(message);
+  }
+
+  setToasterMessage(message){
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
   }
 
 }
--- src/providers/dashboard.ts
+++ src/providers/dashboard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import {HttpClient} from "./http-client/http-client";
+import {Observable} from 'rxjs/Rx';
 
 /*
   Generated class for the Dashboard provider.
@@ -11,8 +11,51 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class Dashboard {
 
-  constructor(public http: Http) {
+  constructor(public httpClient: HttpClient) {
     console.log('Hello Dashboard Provider');
   }
+
+  getAllDashBoardsFromServer(currentUser){
+    let url = "/api/dashboards.json?paging=false&fields=name,id,itemCount,dashboardItems[id,name,interpretationCount," +
+      "interpretationLikeCount,type,shape,chart[id,name,type,series,category,relativePeriods,programIndicatorDimensions," +
+      "dataDimensionItems,dataElementDimensions,periods,organisationUnits,categoryDimensions]]";
+    let self = this;
+    return new Promise(function(resolve, reject) {
+      self.httpClient.get(url,currentUser).subscribe(response=>{
+        resolve(response.json())
+      },error=>{
+        reject(error.json());
+      })
+    });
+  }
+
+  getDefaultDashBoard(type){
+    let dashBoard = {
+      title: { text: 'simple chart' },
+      chart: { type: type },
+      series: [
+        {
+          data: [49.9, 17.5, 10.4]
+        },
+        {
+          data: [6.9, 11.5, 16.4]
+        },
+        {
+          data: [42.9, 30.5, 14.8]
+        },
+        {
+          data: [39.9, 18.5, 20.4]
+        },
+        {
+          data: [16.9, 15.5, 1.4]
+        },
+        {
+          data: [32.9, 3.5, 17.8]
+        }
+      ]
+    }
+    return dashBoard;
+  }
+
 
 }
