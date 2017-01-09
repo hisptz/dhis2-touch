ca88c6e178cd3445977d13ff96df36438c20d432
Joseph Chingalo <profschingalo@gmail.com>
2017-01-09 08:33:26 +0300

    add list of metadate for update

--- src/pages/update-manager-home/update-manager-home.ts
+++ src/pages/update-manager-home/update-manager-home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {ToastController,NavController } from 'ionic-angular';
 
+import {User} from '../../providers/user/user';
+import {HttpClient} from "../../providers/http-client/http-client";
+import {SqlLite} from "../../providers/sql-lite/sql-lite";
 /*
   Generated class for the UpdateManagerHome page.
 
@@ -9,16 +12,60 @@ import { NavController } from 'ionic-angular';
 */
 @Component({
   selector: 'page-update-manager-home',
-  templateUrl: 'update-manager-home.html'
+  templateUrl: 'update-manager-home.html',
+  providers : [User,HttpClient,SqlLite]
 })
 export class UpdateManagerHome {
 
-  constructor(public navCtrl: NavController) {}
+  public dataBaseStructure :  any;
+  public resources : any;
+  public loadingData : boolean = false;
+  public loadingMessages : any = [];
+  public currentUser : any;
+
+
+  constructor(public navCtrl: NavController,public sqlLite : SqlLite,
+              public user : User,public toastCtrl: ToastController,
+              public httpClient : HttpClient) {
+
+
+
+    this.setUpdateManagerList();
+  }
 
   ionViewDidLoad() {
   }
 
+  setUpdateManagerList(){
+    this.resources = [];
+    this.dataBaseStructure = this.sqlLite.getDataBaseStructure();
+    Object.keys(this.dataBaseStructure).forEach((resource:any)=>{
+      if(this.dataBaseStructure[resource].canBeUpdated){
+        this.resources.push({
+          name : resource,
+          displayName : this.getResourceDisplayName(resource),
+          status : false
+        })
+      }
+    });
+  }
 
+  getResourceDisplayName(resourceName){
+    let displayName : string;
+    displayName = (resourceName.charAt(0).toUpperCase() + resourceName.slice(1)).replace(/([A-Z])/g, ' $1').trim();
+    return displayName;
+  }
 
+  setLoadingMessages(message){
+    this.loadingMessages.push(message);
+  }
+
+  setToasterMessage(message){
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
 
 }
