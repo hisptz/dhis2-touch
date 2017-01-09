0101c823191a7319bb974be87c29e8201f4b7d41
Joseph Chingalo <profschingalo@gmail.com>
2017-01-09 12:36:49 +0300

    add service for update resources

--- src/pages/update-manager-home/update-manager-home.ts
+++ src/pages/update-manager-home/update-manager-home.ts
@@ -4,6 +4,8 @@ import {ToastController,NavController } from 'ionic-angular';
 import {User} from '../../providers/user/user';
 import {HttpClient} from "../../providers/http-client/http-client";
 import {SqlLite} from "../../providers/sql-lite/sql-lite";
+import {AppProvider} from "../../providers/app-provider/app-provider";
+import {UpdateResourceManager} from "../../providers/update-resource-manager";
 /*
   Generated class for the UpdateManagerHome page.
 
@@ -13,7 +15,7 @@ import {SqlLite} from "../../providers/sql-lite/sql-lite";
 @Component({
   selector: 'page-update-manager-home',
   templateUrl: 'update-manager-home.html',
-  providers : [User,HttpClient,SqlLite]
+  providers : [User,HttpClient,SqlLite,AppProvider,UpdateResourceManager]
 })
 export class UpdateManagerHome {
 
@@ -22,15 +24,28 @@ export class UpdateManagerHome {
   public loadingData : boolean = false;
   public loadingMessages : any = [];
   public currentUser : any;
-
+  public hasAllSelected : boolean;
+  public hasSelectedResourceUpdated : boolean = false;
 
   constructor(public navCtrl: NavController,public sqlLite : SqlLite,
               public user : User,public toastCtrl: ToastController,
+              public appProvider : AppProvider,public updateResourceManager : UpdateResourceManager,
               public httpClient : HttpClient) {
 
 
+    this.hasAllSelected = false;
+    this.loadingData = true;
+    this.loadingMessages = [];
+    this.setLoadingMessages("Loading current user information");
+    this.user.getCurrentUser().then((user:any)=>{
+      this.currentUser = user;
+      this.user.getUserData().then((userData : any)=>{
+        this.currentUser["organisationUnits"] = userData.organisationUnits;
+        this.setUpdateManagerList();
+        this.loadingData = false;
+      });
+    });
 
-    this.setUpdateManagerList();
   }
 
   ionViewDidLoad() {
@@ -55,6 +70,123 @@ export class UpdateManagerHome {
     displayName = (resourceName.charAt(0).toUpperCase() + resourceName.slice(1)).replace(/([A-Z])/g, ' $1').trim();
     return displayName;
   }
+
+
+  checkingForResourceUpdate(){
+    let canUpdate = false;
+    let listOfResourceToBeUpdate = [];
+    this.resources.forEach((resource:any)=>{
+      if(resource.status){
+        canUpdate = true;
+        listOfResourceToBeUpdate.push(resource);
+      }
+    });
+    if(canUpdate){
+      this.updateResources(listOfResourceToBeUpdate);
+    }else{
+      this.setToasterMessage("Please select at least one resources to update")
+    }
+  }
+
+
+  updateResources(resources){
+    this.loadingData = true;
+    this.loadingMessages = [];
+    this.setLoadingMessages("Downloading updates");
+    this.updateResourceManager.downloadResources(resources,this.currentUser).then((resourcesData)=>{
+      this.setLoadingMessages("Applying updates");
+      this.updateResourceManager.savingResources(resourcesWithData,resourcesData,this.currentUser).then(()=>{
+        this.loadingData = false;
+      },error=>{
+        this.loadingData = false;
+        this.setToasterMessage("Fail to apply updates : " + JSON.stringify(error));
+      });
+    },error=>{
+      this.loadingData = false;
+      this.setToasterMessage("Fail to download updates : " + JSON.stringify(error));
+    });
+  }
+
+  updateResourcessds(resources){
+    //hasSelectedResourceUpdated
+    let numberOfDownload : number = 0;
+    let numberOfUpdateResources : number = 0;
+    this.loadingData = true;
+    this.loadingMessages = [];
+    this.setLoadingMessages("Download percent : 0 %");
+    this.setLoadingMessages("Update percent : 0 %" );
+    for(let resource of resources){
+      let resourceName = resource.name;
+      let fields = this.dataBaseStructure[resourceName].fields;
+      let filter = this.dataBaseStructure[resourceName].filter;
+      if(resourceName != "organisationUnits"){
+        this.setLoadingMessages("Downloading " + resource.displayName);
+        this.appProvider.downloadMetadata(this.currentUser,resourceName,null,fields,filter).then((response : any) =>{
+          numberOfDownload ++;
+          this.loadingMessages[0] = "Download percent : " +this.getPercentage(numberOfDownload,resources.length)+" %";
+          this.setLoadingMessages("Updating " + resource.displayName);
+          this.appProvider.saveMetadata(resourceName,response[resourceName],this.currentUser.currentDatabase).then(()=>{
+            numberOfUpdateResources ++;
+            this.loadingMessages[1] = "Update percent : " +this.getPercentage(numberOfUpdateResources,resources.length)+" %";
+            if(numberOfUpdateResources == resources.length){
+              this.hasSelectedResourceUpdated = true;
+              this.loadingData = false;
+            }
+          },error=>{
+            this.setToasterMessage("Fail to update " + resource.displayName + " : " + JSON.stringify(error));
+          })
+        },error=>{
+          this.loadingData = false;
+          this.setToasterMessage("Fail to download " + resource.displayName + " : " + JSON.stringify(error));
+        })
+      }else{
+        let orgUnitIds = [];
+        this.currentUser["organisationUnits"].forEach((orgUnit:any)=>{
+          orgUnitIds.push(orgUnit.id);
+        });
+        this.setLoadingMessages("Downloading " + resource.displayName);
+        this.appProvider.downloadMetadataByResourceIds(this.currentUser,resourceName,orgUnitIds,fields,filter).then((response : any) =>{
+          numberOfDownload ++;
+          this.loadingMessages[0] = "Download percent : " +this.getPercentage(numberOfDownload,resources.length)+" %";
+          this.setLoadingMessages("Updating " + resource.displayName);
+          this.appProvider.saveMetadata(resourceName,response,this.currentUser.currentDatabase).then(()=>{
+            numberOfUpdateResources ++;
+            this.loadingMessages[1] = "Update percent : " +this.getPercentage(numberOfUpdateResources,resources.length)+" %";
+            if(numberOfUpdateResources == resources.length){
+              this.hasSelectedResourceUpdated = true;
+              this.loadingData = false;
+            }
+          },error=>{
+            this.setToasterMessage("Fail to update " + resource.displayName + " : " + JSON.stringify(error));
+          })
+        },error=>{
+          this.loadingData = false;
+          this.setToasterMessage("Fail to download " + resource.displayName + " : " + JSON.stringify(error));
+        })
+      }
+    }
+  }
+
+  getPercentage(numerator : number,denominator : number){
+    let fraction : number;
+    fraction = (numerator/denominator) *100;
+    return fraction.toFixed(2)
+  }
+
+  autoSelect(selectType){
+    if(selectType == 'selectAll'){
+      this.resources.forEach((resource:any)=>{
+        resource.status = true;
+      });
+      this.hasAllSelected = true;
+    }else{
+      this.resources.forEach((resource:any)=>{
+        resource.status = false;
+      });
+      this.hasAllSelected = false;
+    }
+  }
+
 
   setLoadingMessages(message){
     this.loadingMessages.push(message);
---
+++ src/providers/update-resource-manager.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+import {AppProvider} from "./app-provider/app-provider";
+import {SqlLite} from "./sql-lite/sql-lite";
+import {HttpClient} from "./http-client/http-client";
+
+/*
+  Generated class for the UpdateResourceManager provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class UpdateResourceManager {
+
+  public dataBaseStructure : any;
+
+  constructor(public HttpClient : HttpClient,public sqlLite : SqlLite,public appProvider : AppProvider) {
+    this.dataBaseStructure = this.sqlLite.getDataBaseStructure();
+  }
+
+  downloadResources(resources,currentUser){
+    let self = this;
+    let promises = [];
+    let data  = {};
+    return new Promise(function(resolve, reject) {
+      resources.forEach((resource:any)=>{
+        let resourceName = resource.name;
+        let fields = self.dataBaseStructure[resourceName].fields;
+        let filter = self.dataBaseStructure[resourceName].filter;
+        if(resourceName != "organisationUnits"){
+          promises.push(
+            self.appProvider.downloadMetadata(currentUser,resourceName,null,fields,filter).then((response : any) =>{
+              data[resource.name] = response;
+              //resource["response"] = response;
+            },error=>{})
+          );
+        }else{
+          let orgUnitIds = [];
+          currentUser["organisationUnits"].forEach((orgUnit:any)=>{
+            orgUnitIds.push(orgUnit.id);
+          });
+          promises.push(
+            self.appProvider.downloadMetadataByResourceIds(currentUser,resourceName,orgUnitIds,fields,filter).then((response : any) =>{
+              //resource["response"] = response;
+              data[resource.name] = response;
+            },error=>{})
+          );
+        }
+      });
+
+      Observable.forkJoin(promises).subscribe(() => {
+          resolve(data);
+        },
+        (error) => {
+          reject(error);
+        })
+    });
+  }
+
+  savingResources(resources,data,currentUser){
+    let self = this;
+    let promises = [];
+
+    return new Promise(function(resolve, reject) {
+      resourcesWithData.forEach((resource:any)=>{
+        let resourceName = resource.name;
+        promises.push(
+          self.appProvider.saveMetadata(resourceName,data[resourceName],currentUser.currentDatabase).then((
+          )=>{
+          },error=>{});
+        );
+      });
+
+      Observable.forkJoin(promises).subscribe(() => {
+          resolve();
+        },
+        (error) => {
+          reject(error);
+        })
+    });
+  }
+
+}
