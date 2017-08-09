94f1aa8d2aa026ba0f84a4c5269a660b94f64d49
Joseph Chingalo <profschingalo@gmail.com>
2017-08-09 12:31:20 +0300

    update style and contents for about page

--- src/providers/about/about.ts
+++ src/providers/about/about.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {AppProvider} from "../app/app";
 import {UserProvider} from "../user/user";
 
@@ -11,52 +11,52 @@ import {UserProvider} from "../user/user";
 @Injectable()
 export class AboutProvider {
 
-  constructor(private appProvider: AppProvider, private userProvider: UserProvider) {
+  constructor(private appProvider : AppProvider,private userProvider : UserProvider) {
   }
 
-  getAboutContentDetails() {
-    let aboutContents = [
+  getAboutContentDetails(){
+    let syncContents = [
       //{id : 'appInformation',name : 'App information',icon: 'assets/about-icons/app-information.png'},
-      {id: 'dataValues', name: 'Data values', icon: 'assets/about-icons/data-values.png'},
-      {id: 'eventStatus', name: 'Event storage status', icon: 'assets/about-icons/event-status.png'},
-      {id: 'systemInfo', name: 'System info', icon: 'assets/about-icons/system-info.png'},
+      {id : 'dataValues',name : 'Data values',icon: 'assets/about-icons/data-values.png'},
+      {id : 'eventStatus',name : 'Event storage status',icon: 'assets/about-icons/event-status.png'},
+      {id : 'systemInfo',name : 'System info',icon: 'assets/about-icons/system-info.png'},
     ];
-    return aboutContents;
+    return syncContents;
   }
 
-  getAppInformation() {
-    let appInformation = {name: '', version: '', package: ''};
+  getAppInformation(){
+    let appInformation = {name : '',version : '',package : ''};
     return new Promise((resolve, reject) => {
-      this.appProvider.getAppInformation().then((response: any) => {
+      this.appProvider.getAppInformation().then((response : any)=>{
         appInformation.name = response.appName;
         appInformation.version = response.versionNumber;
         appInformation.package = response.packageName;
         resolve(appInformation);
-      }, error => {
+      },error=>{
         reject(error);
       });
     });
   }
 
-  getSystemInformation() {
-    return new Promise((resolve, reject) => {
-      this.userProvider.getCurrentUserSystemInformation().then(systemInfo => {
+  getSystemInformation(){
+    return new Promise((resolve,reject) => {
+      this.userProvider.getCurrentUserSystemInformation().then(systemInfo=>{
         resolve(this.getArrayFromObject(systemInfo));
-      }).catch(error => {
+      }).catch(error=>{
         reject(error);
       });
     });
   }
 
-  getArrayFromObject(object) {
+  getArrayFromObject(object){
     let array = [];
-    for (let key in object) {
+    for(let key in object){
       let newValue = object[key];
-      if (newValue instanceof Object) {
+      if(newValue instanceof Object) {
         newValue = JSON.stringify(newValue)
       }
       let newKey = (key.charAt(0).toUpperCase() + key.slice(1)).replace(/([A-Z])/g, ' $1').trim();
-      array.push({key: newKey, value: newValue})
+      array.push({key : newKey,value : newValue})
     }
     return array;
   }
