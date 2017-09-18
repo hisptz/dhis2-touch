dc2c9163f8acc533276b2a5e8e8fadde8d889d78
Joseph Chingalo <profschingalo@gmail.com>
2017-09-18 17:22:13 +0300

    reset download data component

--- src/components/download-data/download-data.ts
+++ src/components/download-data/download-data.ts
@@ -1,9 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {UserProvider} from "../../providers/user/user";
-import {AppProvider} from "../../providers/app/app";
-import {SqlLiteProvider} from "../../providers/sql-lite/sql-lite";
-import {AlertController, NavController, NavParams} from "ionic-angular";
-import {SyncProvider} from "../../providers/sync/sync";
 
 /**
  * Generated class for the DownloadDataComponent component.
@@ -17,49 +12,13 @@ import {SyncProvider} from "../../providers/sync/sync";
 })
 export class DownloadDataComponent implements OnInit{
 
-  text: string;
-  currentUser: any;
 
-  isDownloadContentOpen : any;
-  downloadContents : Array<any>;
-
-
-  downloadManagerObject : any = {
-    dataDownload : {isExpanded : false,isProcessRunning : false,isDataDownloaded : true,selectedItems :{}, itemsToBeDownloaded : []}
-
-  };
-
-
-  constructor(public alertCtrl: AlertController, private sqLite: SqlLiteProvider,
-              private appProvider: AppProvider, public user: UserProvider,
-              public navCtrl: NavController, public navParams: NavParams,private syncProvider : SyncProvider) {
+  constructor() {
 
   }
-
-
 
   ngOnInit(){
-    this.isDownloadContentOpen = {};
-    this.downloadContents = this.syncProvider.getDownloadDataDetails();
-    if(this.downloadContents.length > 0){
-      this.toggleDownloadContents(this.downloadContents[0]);
-    }
-  }
-
-  toggleDownloadContents(content){
-    if(content && content.id){
-      if(this.isDownloadContentOpen[content.id]){
-        this.isDownloadContentOpen[content.id] = false;
-      }else{
-        Object.keys(this.isDownloadContentOpen).forEach(id=>{
-          this.isDownloadContentOpen[id] = false;
-        });
-        this.isDownloadContentOpen[content.id] = true;
-      }
-    }
 
   }
-
-
 
 }
--- src/pages/sync/sync.module.ts
+++ src/pages/sync/sync.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SyncPage } from './sync';
 import {SyncModule} from "../../components/sync.module";
+import {SharedModule} from "../../components/shared.module";
 
 @NgModule({
   declarations: [
     SyncPage,
   ],
   imports: [
-    IonicPageModule.forChild(SyncPage),SyncModule
+    IonicPageModule.forChild(SyncPage),SyncModule,SharedModule
   ],
 })
 export class SyncPageModule {}
