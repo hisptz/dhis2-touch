9c5186aa5f614cae5fd6c2f4a0459f832d4d887a
Joseph Chingalo <profschingalo@gmail.com>
2017-10-20 12:47:55 +0300

    Implement logics for event dates during regitration of new event

--- src/components/program-stage-event-based/program-stage-event-based.ts
+++ src/components/program-stage-event-based/program-stage-event-based.ts
@@ -25,26 +25,52 @@ export class ProgramStageEventBasedComponent implements OnInit, OnDestroy{
   currentUser : any;
   isLoading : boolean;
   loadingMessage : string;
-  dataObject : any;
+  dataObjectModel : any;
+  eventDate : any;
 
   constructor(private programsProvider : ProgramsProvider,
+
               private userProvider : UserProvider,private appProvider : AppProvider,
               private organisationUnitProvider : OrganisationUnitsProvider) {
   }
 
   ngOnInit(){
-    this.dataObject = {};
+    this.dataObjectModel = {};
     this.currentOrgUnit = this.organisationUnitProvider.lastSelectedOrgUnit;
     this.currentProgram = this.programsProvider.lastSelectedProgram;
     this.loadingMessage = "Loading user information";
     this.isLoading = true;
     this.userProvider.getCurrentUser().then((user : any)=>{
       this.currentUser = user;
+      if(this.currentEvent && this.currentEvent.dataValues && this.currentEvent.dataValues.length){
+        this.updateDataObjectModel(this.currentEvent.dataValues,this.programStage.programStageDataElements);
+      }
+      if(this.currentEvent.eventDate){
+        this.eventDate = this.currentEvent.eventDate;
+      }
       this.isLoading = false;
     }).catch(error=>{
       this.isLoading = false;
       this.appProvider.setNormalNotification("Fail to load user information");
     })
+  }
+
+  updateDataObjectModel(dataValues,programStageDataElements){
+    let dataValuesMapper = {};
+    dataValues.forEach((dataValue : any)=>{
+      dataValuesMapper[dataValue.dataElement] = dataValue.value;
+    });
+    programStageDataElements.forEach((programStageDataElement : any)=>{
+      if(programStageDataElement.dataElement && programStageDataElements.dataElement.id){
+        if(dataValuesMapper[programStageDataElements.dataElement.id]){
+          this.dataObjectModel[programStageDataElements.dataElement.id] = dataValuesMapper[programStageDataElements.dataElement.id];
+        }
+      }
+    });
+  }
+
+  updateEventDate(){
+    console.log(this.eventDate);
   }
 
   ngOnDestroy(){
