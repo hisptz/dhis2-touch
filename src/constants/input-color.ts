a761e1327dd26dc54fb6e0cd3d2867bb65a06ab1
Joseph Chingalo <profschingalo@gmail.com>
2017-11-19 05:15:23 +0300

    Adding color constants for app

---
+++ src/constants/input-color.ts
@@ -0,0 +1,10 @@
+export const INPUT_COLOR = {
+  colorGreen : '#b9ffb9',
+  colorYellow : '#fffe8c',
+  colorRed : '#ff8a8a',
+  colorOrange : '#ff6600',
+  colorWhite : '#fff',
+  colorGrey : '#ccc',
+  colorBorderActive : '#73ad72',
+  colorBorder : '#aaa',
+};
