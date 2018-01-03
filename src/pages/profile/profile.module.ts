711a663dfd3b8ce2521999e0d8c50356e0761d33
Joseph Chingalo <profschingalo@gmail.com>
2018-01-03 17:00:44 +0300

    Update about app

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -2,22 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfilePage } from './profile';
 import {SharedModule} from "../../components/shared.module";
-import {Http} from "@angular/http";
-import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
-import {createTranslateLoader} from "../../app/app.module";
+import { TranslateModule} from "@ngx-translate/core";
 @NgModule({
   declarations: [
     ProfilePage,
   ],
   imports: [
     IonicPageModule.forChild(ProfilePage),SharedModule,
-    TranslateModule.forChild({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [Http]
-      }
-    })
+    TranslateModule.forChild({})
   ],
 })
 export class ProfilePageModule {}
