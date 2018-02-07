adee07f53711b0aa8d12c246eb930201676086e6
Joseph Chingalo <profschingalo@gmail.com>
2018-02-07 15:19:08 +0300

    Message notification in case data set report has no data element setup

--- src/components/report.module.ts
+++ src/components/report.module.ts
@@ -1,16 +1,12 @@
-import {NgModule} from '@angular/core';
-import {IonicModule} from 'ionic-angular';
-import {DataSetReportComponent} from "./data-set-report/data-set-report";
-import {SharedModule} from "./shared.module";
-import {DataSetReportRowComponent} from "./data-set-report-row/data-set-report-row";
+import { NgModule } from "@angular/core";
+import { IonicModule } from "ionic-angular";
+import { DataSetReportComponent } from "./data-set-report/data-set-report";
+import { SharedModule } from "./shared.module";
+import { DataSetReportRowComponent } from "./data-set-report-row/data-set-report-row";
 
 @NgModule({
   declarations: [DataSetReportComponent, DataSetReportRowComponent],
-  imports: [
-    IonicModule, SharedModule,
-  ],
+  imports: [IonicModule, SharedModule],
   exports: [DataSetReportComponent, DataSetReportRowComponent]
 })
-
-export class ReportModule {
-}
+export class ReportModule {}
