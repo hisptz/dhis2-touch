af159ad9713c72bc91e8bfe06f317b39066b8ef9
Joseph Chingalo <profschingalo@gmail.com>
2018-02-21 10:48:40 +0300

    Update translations for apps and utility app list

--- src/components/data-set-report/data-set-report.ts
+++ src/components/data-set-report/data-set-report.ts
@@ -1,8 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AppProvider} from "../../providers/app/app";
-import {DataSetReportProvider} from "../../providers/data-set-report/data-set-report";
-import {DataEntryFormProvider} from "../../providers/data-entry-form/data-entry-form";
-import {SettingsProvider} from "../../providers/settings/settings";
+import { Component, Input, OnInit } from "@angular/core";
+import { AppProvider } from "../../providers/app/app";
+import { DataSetReportProvider } from "../../providers/data-set-report/data-set-report";
+import { DataEntryFormProvider } from "../../providers/data-entry-form/data-entry-form";
+import { SettingsProvider } from "../../providers/settings/settings";
+import { AppTranslationProvider } from "../../providers/app-translation/app-translation";
 
 /**
  * Generated class for the DataSetReportComponent component.
@@ -11,74 +12,122 @@ import {SettingsProvider} from "../../providers/settings/settings";
  * Components.
  */
 @Component({
-  selector: 'data-set-report',
-  templateUrl: 'data-set-report.html'
+  selector: "data-set-report",
+  templateUrl: "data-set-report.html"
 })
-export class DataSetReportComponent implements OnInit{
-
+export class DataSetReportComponent implements OnInit {
   @Input() dataSetId;
   @Input() selectedPeriod;
   @Input() selectedOrganisationUnit;
   @Input() currentUser;
 
-  isLoading : boolean;
-  loadingMessage : string;
-  dataSet : any;
-  sectionIds : any;
-  appSettings : any;
-  entryFormSections : any;
-  dataElementDataValuesMapper : any;
+  isLoading: boolean;
+  loadingMessage: string;
+  dataSet: any;
+  sectionIds: any;
+  appSettings: any;
+  entryFormSections: any;
+  dataElementDataValuesMapper: any;
+  translationMapper: any;
 
-  constructor(private appProvider : AppProvider,
-              private settingsProvider : SettingsProvider,
-              private dataEntryFormProvider : DataEntryFormProvider,
-              private dataSetReportProvider : DataSetReportProvider) {
-  }
+  constructor(
+    private appProvider: AppProvider,
+    private settingsProvider: SettingsProvider,
+    private dataEntryFormProvider: DataEntryFormProvider,
+    private dataSetReportProvider: DataSetReportProvider,
+    private appTranslation: AppTranslationProvider
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.isLoading = true;
     this.dataElementDataValuesMapper = {};
-    if(this.currentUser){
-      this.settingsProvider.getSettingsForTheApp(this.currentUser).subscribe((appSettings:any)=>{
-        this.appSettings = appSettings;
-        this.loadingMessage = "loading_data_set_report_information";
-        this.dataEntryFormProvider.loadingDataSetInformation(this.dataSetId,this.currentUser).subscribe((dataSetInformation : any)=>{
-          this.dataSet = dataSetInformation.dataSet;
-          this.sectionIds = dataSetInformation.sectionIds;
-          this.loadingMessage = "prepare_data_set_report";
-          this.dataEntryFormProvider.getEntryForm(this.sectionIds,this.dataSet.id,this.appSettings,this.currentUser).subscribe((entryFormSections : any)=>{
-            entryFormSections.forEach((section : any)=>{
-              if(this.sectionIds.length == 0){
-                section.name = "";
+    this.translationMapper = {};
+    if (this.currentUser) {
+      this.settingsProvider
+        .getSettingsForTheApp(this.currentUser)
+        .subscribe((appSettings: any) => {
+          this.appSettings = appSettings;
+          let key = "Discovering data set report information";
+          this.loadingMessage = this.translationMapper[key]
+            ? this.translationMapper[key]
+            : key;
+          this.dataEntryFormProvider
+            .loadingDataSetInformation(this.dataSetId, this.currentUser)
+            .subscribe(
+              (dataSetInformation: any) => {
+                this.dataSet = dataSetInformation.dataSet;
+                this.sectionIds = dataSetInformation.sectionIds;
+                key = "Preparing data set report";
+                this.loadingMessage = this.translationMapper[key]
+                  ? this.translationMapper[key]
+                  : key;
+                this.dataEntryFormProvider
+                  .getEntryForm(
+                    this.sectionIds,
+                    this.dataSet.id,
+                    this.appSettings,
+                    this.currentUser
+                  )
+                  .subscribe(
+                    (entryFormSections: any) => {
+                      entryFormSections.forEach((section: any) => {
+                        if (this.sectionIds.length == 0) {
+                          section.name = "";
+                        }
+                        section.dataElements.forEach((dataElement: any) => {
+                          this.dataElementDataValuesMapper[dataElement.id] = [];
+                        });
+                      });
+                      this.entryFormSections = entryFormSections;
+                      this.dataSetReportProvider
+                        .getReportValues(
+                          this.selectedOrganisationUnit,
+                          this.dataSetId,
+                          this.selectedPeriod.iso,
+                          this.currentUser
+                        )
+                        .subscribe(
+                          (dataValuesResponse: any) => {
+                            dataValuesResponse.forEach((dataValue: any) => {
+                              this.dataElementDataValuesMapper[
+                                dataValue.de
+                              ].push(dataValue);
+                            });
+                            this.isLoading = false;
+                          },
+                          error => {
+                            this.isLoading = false;
+                          }
+                        );
+                    },
+                    error => {
+                      this.isLoading = false;
+                      this.loadingMessage = "";
+                      this.appProvider.setNormalNotification(
+                        "Fail to prepare data set report"
+                      );
+                    }
+                  );
+              },
+              error => {
+                this.isLoading = false;
+                this.loadingMessage = "";
+                this.appProvider.setNormalNotification(
+                  "Fail to discover data set report information"
+                );
               }
-              section.dataElements.forEach((dataElement : any)=>{
-                this.dataElementDataValuesMapper[dataElement.id] = [];
-              });
-            });
-            this.entryFormSections = entryFormSections;
-            this.dataSetReportProvider.getReportValues(this.selectedOrganisationUnit,this.dataSetId,this.selectedPeriod.iso,this.currentUser).subscribe((dataValuesResponse: any)=>{
-              dataValuesResponse.forEach((dataValue : any)=>{
-                this.dataElementDataValuesMapper[dataValue.de].push(dataValue);
-              });
-              this.isLoading = false;
-            },error=>{
-              this.isLoading = false;
-            });
-          },error=>{
-            this.isLoading = false;
-            this.loadingMessage = "";
-            this.appProvider.setNormalNotification("Fail to prepare data set report");
-          })
-        },error=>{
-          this.isLoading = false;
-          this.loadingMessage = "";
-          this.appProvider.setNormalNotification("Fail to load data set report information");
-        })
-      });
-
+            );
+        });
     }
   }
 
+  loadingDataSetInformation() {}
 
-
+  getValuesTranslate() {
+    return [
+      "Discovering data set report information",
+      "Preparing data set report",
+      "Missing entry form sections, please contact administarator"
+    ];
+  }
 }
