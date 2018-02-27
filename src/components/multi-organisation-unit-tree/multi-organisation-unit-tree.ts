00e9978014f6c32c3996cc813ad79109af4b0a51
Joseph Chingalo <profschingalo@gmail.com>
2018-02-27 16:21:08 +0300

    Adding capability to open mutltiple selected ou

--- src/components/multi-organisation-unit-tree/multi-organisation-unit-tree.ts
+++ src/components/multi-organisation-unit-tree/multi-organisation-unit-tree.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { OrganisationUnitsProvider } from '../../providers/organisation-units/organisation-units';
 
 /**
@@ -11,19 +11,51 @@ import { OrganisationUnitsProvider } from '../../providers/organisation-units/or
   selector: 'multi-organisation-unit-tree',
   templateUrl: 'multi-organisation-unit-tree.html'
 })
-export class MultiOrganisationUnitTreeComponent {
+export class MultiOrganisationUnitTreeComponent implements OnInit {
   @Input() toggledOuIds;
   @Input() organisationUnit;
+  @Input() selectedOrgUnits;
   @Input() currentUser;
 
   constructor(private organisationUnitProvider: OrganisationUnitsProvider) {}
+
+  ngOnInit() {
+    if (this.selectedOrgUnits && this.selectedOrgUnits.length > 0) {
+      let ids = [];
+      this.selectedOrgUnits.map((ou: any) => {
+        ids.push(ou.id);
+      });
+      this.organisationUnitProvider
+        .getOrganisationUnitsByIds(ids, this.currentUser)
+        .subscribe((selectedOrganisationUnits: any) => {
+          selectedOrganisationUnits.map((selectedOrganisationUnit: any) => {
+            let parentCopy = selectedOrganisationUnit.path
+              .substring(1, selectedOrganisationUnit.path.length)
+              .split('/');
+            if (parentCopy.indexOf(this.organisationUnit.id) > -1) {
+              selectedOrganisationUnit.ancestors.forEach((ancestor: any) => {
+                if (ancestor.id == this.organisationUnit.id) {
+                  console.log('expand');
+                  this.expandTree(ancestor);
+                }
+              });
+            }
+          });
+        });
+    }
+  }
 
   toggleTree(organisationUnit) {
     let ouIndex = this.toggledOuIds.indexOf(organisationUnit.id);
     if (ouIndex > -1) {
       this.toggledOuIds.splice(ouIndex, 1);
     } else {
-      this.toggledOuIds.push(organisationUnit.id);
+      this.expandTree(organisationUnit);
+    }
+  }
+
+  expandTree(organisationUnit) {
+    if (this.toggledOuIds.indexOf(organisationUnit.id) == -1) {
       let childrenOrganisationUnitIds = this.getOrganisationUnitsChildrenIds(
         organisationUnit
       );
@@ -36,6 +68,7 @@ export class MultiOrganisationUnitTreeComponent {
           (childrenOrganisationUnits: any) => {
             if (this.organisationUnit && this.organisationUnit.children) {
               this.organisationUnit.children = childrenOrganisationUnits;
+              this.toggledOuIds.push(organisationUnit.id);
             }
           },
           error => {
--- src/components/multi-organisation-unit/multi-organisation-unit.ts
+++ src/components/multi-organisation-unit/multi-organisation-unit.ts
@@ -14,11 +14,35 @@ export class MultiOrganisationUnitComponent implements OnInit {
   @Input() organisationUnits;
   @Input() currentUser;
 
+  selectedOrgUnits: any;
+
   toggledOuIds: Array<string>;
 
   constructor() {}
 
   ngOnInit() {
     this.toggledOuIds = [];
+    this.selectedOrgUnits = [
+      {
+        id: 'l0ccv2yzfF3',
+        name: 'Kunike',
+        type: 'ORGANISATION_UNIT'
+      },
+      {
+        name: 'Adonkia CHP',
+        type: 'ORGANISATION_UNIT',
+        id: 'Rp268JB6Ne4'
+      },
+      {
+        name: 'Afro Arab Clinic',
+        type: 'ORGANISATION_UNIT',
+        id: 'cDw53Ej8rju'
+      },
+      {
+        name: 'Agape CHP',
+        type: 'ORGANISATION_UNIT',
+        id: 'GvFqTavdpGE'
+      }
+    ];
   }
 }
