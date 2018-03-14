88099507ee80f9bce64b72fbe51fb55468b04542
Joseph Chingalo <profschingalo@gmail.com>
2018-03-14 15:33:41 +0300

    Adding filter types on orgunit tree selections for programs and data set assignments

--- src/pages/organisation-unit-selection/organisation-unit-selection.ts
+++ src/pages/organisation-unit-selection/organisation-unit-selection.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { IonicPage, ViewController } from "ionic-angular";
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, ViewController, NavParams } from 'ionic-angular';
 import {
   OrganisationUnitModel,
   OrganisationUnitsProvider
-} from "../../providers/organisation-units/organisation-units";
-import { UserProvider } from "../../providers/user/user";
-import { AppProvider } from "../../providers/app/app";
-import { AppTranslationProvider } from "../../providers/app-translation/app-translation";
+} from '../../providers/organisation-units/organisation-units';
+import { UserProvider } from '../../providers/user/user';
+import { AppProvider } from '../../providers/app/app';
+import { AppTranslationProvider } from '../../providers/app-translation/app-translation';
+import { ProgramsProvider } from '../../providers/programs/programs';
+import { DataSetsProvider } from '../../providers/data-sets/data-sets';
 
 /**
  * Generated class for the OrganisationUnitSelectionPage page.
@@ -17,8 +19,8 @@ import { AppTranslationProvider } from "../../providers/app-translation/app-tran
 
 @IonicPage()
 @Component({
-  selector: "page-organisation-unit-selection",
-  templateUrl: "organisation-unit-selection.html"
+  selector: 'page-organisation-unit-selection',
+  templateUrl: 'organisation-unit-selection.html'
 })
 export class OrganisationUnitSelectionPage implements OnInit {
   loadingMessage: string;
@@ -29,19 +31,24 @@ export class OrganisationUnitSelectionPage implements OnInit {
   lastSelectedOrgUnit: OrganisationUnitModel;
   organisationUnits: OrganisationUnitModel[];
   hasOrgUnitChildrenOpened: any = {};
+  ouIdsWithAssigments: Array<string>;
 
   constructor(
     private viewCtrl: ViewController,
     private organisationUnitProvider: OrganisationUnitsProvider,
+    private programProvider: ProgramsProvider,
+    private dataSetsProvider: DataSetsProvider,
+    private navParams: NavParams,
     private userProvider: UserProvider,
     private appProvider: AppProvider,
     private appTranslation: AppTranslationProvider
   ) {}
 
   ngOnInit() {
-    this.emptyMessage = "";
+    this.emptyMessage = '';
     this.isLoading = true;
     this.translationMapper = {};
+    this.ouIdsWithAssigments = [];
     this.appTranslation.getTransalations(this.getValuesToTranslate()).subscribe(
       (data: any) => {
         this.translationMapper = data;
@@ -54,7 +61,7 @@ export class OrganisationUnitSelectionPage implements OnInit {
   }
 
   loadingCurrentUserInformation() {
-    let key = "Discovering current user information";
+    let key = 'Discovering current user information';
     this.loadingMessage = this.translationMapper[key]
       ? this.translationMapper[key]
       : key;
@@ -70,7 +77,7 @@ export class OrganisationUnitSelectionPage implements OnInit {
   }
 
   loadingOrganisationUnits() {
-    let key = "Discovering assigned organisation units";
+    let key = 'Discovering assigned organisation units';
     this.loadingMessage = this.translationMapper[key]
       ? this.translationMapper[key]
       : key;
@@ -89,11 +96,11 @@ export class OrganisationUnitSelectionPage implements OnInit {
                 organisationUnits[0]
               );
             }
-            this.isLoading = false;
+            this.loadingProgramAndDataSetAssignments();
           } else {
             this.isLoading = false;
             key =
-              "Currently there is on assigned organisation unit on local storage, Please metadata on sync app";
+              'Currently there is on assigned organisation unit on local storage, Please metadata on sync app';
             this.emptyMessage = this.translationMapper[key]
               ? this.translationMapper[key]
               : key;
@@ -103,13 +110,24 @@ export class OrganisationUnitSelectionPage implements OnInit {
         error => {
           console.log(JSON.stringify(error));
           this.isLoading = false;
-          key = "Fail to discover organisation units";
+          key = 'Fail to discover organisation units';
           this.emptyMessage = this.translationMapper[key]
             ? this.translationMapper[key]
             : key;
           this.appProvider.setNormalNotification(key);
         }
       );
+  }
+
+  loadingProgramAndDataSetAssignments() {
+    const filterType = this.navParams.get('filterType');
+    alert('filterType : ' + filterType);
+
+    //event : WITHOUT_REGISTRATION
+    //data entry : dataSets
+    //tracker : WITH_REGISTRATION
+
+    this.isLoading = false;
   }
 
   setSelectedOrganisationUnit(selectedOrganisationUnit) {
@@ -125,10 +143,10 @@ export class OrganisationUnitSelectionPage implements OnInit {
 
   getValuesToTranslate() {
     return [
-      "Discovering current user information",
-      "Discovering assigned organisation units",
-      "Currently there is on assigned organisation unit on local storage, Please metadata on sync app",
-      "Fail to discover organisation units"
+      'Discovering current user information',
+      'Discovering assigned organisation units',
+      'Currently there is on assigned organisation unit on local storage, Please metadata on sync app',
+      'Fail to discover organisation units'
     ];
   }
 }
