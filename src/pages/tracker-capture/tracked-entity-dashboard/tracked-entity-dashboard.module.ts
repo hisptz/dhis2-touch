8f7385742fb8b3409bac89d8e5a5cae64d540d41
Joseph Chingalo <profschingalo@gmail.com>
2018-05-16 12:13:00 +0300

    Pass custome entry form to custom entry form components

--- src/pages/tracker-capture/tracked-entity-dashboard/tracked-entity-dashboard.module.ts
+++ src/pages/tracker-capture/tracked-entity-dashboard/tracked-entity-dashboard.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TrackedEntityDashboardPage } from './tracked-entity-dashboard';
-import {SharedModule} from "../../../components/shared.module";
-import {DataEntryModule} from "../../../components/data.entry.module";
-import { TranslateModule} from "@ngx-translate/core";
+import { SharedModule } from '../../../components/shared.module';
+import { DataEntryModule } from '../../../components/data.entry.module';
+import { TranslateModule } from '@ngx-translate/core';
+import { DataEntryComponentsModule } from '../../data-entry/components/dataEntryComponents.module';
 @NgModule({
-  declarations: [
-    TrackedEntityDashboardPage,
-  ],
+  declarations: [TrackedEntityDashboardPage],
   imports: [
-    IonicPageModule.forChild(TrackedEntityDashboardPage),SharedModule,DataEntryModule,
+    IonicPageModule.forChild(TrackedEntityDashboardPage),
+    SharedModule,
+    DataEntryModule,
+    DataEntryComponentsModule,
     TranslateModule.forChild({})
-  ],
+  ]
 })
 export class TrackedEntityDashboardPageModule {}
--- src/pages/tracker-capture/tracker-entity-register/tracker-entity-register.module.ts
+++ src/pages/tracker-capture/tracker-entity-register/tracker-entity-register.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from "@angular/core";
-import { IonicPageModule } from "ionic-angular";
-import { TrackerEntityRegisterPage } from "./tracker-entity-register";
-import { SharedModule } from "../../../components/shared.module";
-import { DataEntryModule } from "../../../components/data.entry.module";
-import { TranslateModule } from "@ngx-translate/core";
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TrackerEntityRegisterPage } from './tracker-entity-register';
+import { SharedModule } from '../../../components/shared.module';
+import { DataEntryModule } from '../../../components/data.entry.module';
+import { TranslateModule } from '@ngx-translate/core';
+import { DataEntryComponentsModule } from '../../data-entry/components/dataEntryComponents.module';
 @NgModule({
   declarations: [TrackerEntityRegisterPage],
   imports: [
     IonicPageModule.forChild(TrackerEntityRegisterPage),
     SharedModule,
     DataEntryModule,
+    DataEntryComponentsModule,
     TranslateModule.forChild({})
   ]
 })
