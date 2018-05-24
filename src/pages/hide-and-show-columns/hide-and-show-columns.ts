b3e97f370c8efc0a4c75705c77cac1512c7a7df8
Joseph Chingalo <profschingalo@gmail.com>
2018-05-24 16:36:10 +0300

    Sanitize all metadata for hide and show visible header

--- src/pages/hide-and-show-columns/hide-and-show-columns.ts
+++ src/pages/hide-and-show-columns/hide-and-show-columns.ts
@@ -17,9 +17,14 @@ import * as _ from 'lodash';
 export class HideAndShowColumnsPage implements OnInit, OnDestroy {
   selectedItemsModel: any;
   fieldsToDisplay: any;
+  displayInLIst: any;
+  isLoading: boolean;
 
   constructor(private navParams: NavParams, private viewCtrl: ViewController) {
     this.fieldsToDisplay = [];
+    this.displayInLIst = [];
+    this.selectedItemsModel = {};
+    this.isLoading = true;
   }
 
   ngOnInit() {
@@ -28,29 +33,94 @@ export class HideAndShowColumnsPage implements OnInit, OnDestroy {
     const { programStage } = data;
     const { programTrackedEntityAttributes } = data;
     const { dataEntrySettings } = data;
-
+    //select all preselected items
     if (columnsToDisplay) {
       _.keys(columnsToDisplay).map(key => {
         this.selectedItemsModel[key] = true;
       });
     }
+    //for events capture table list header
+    if (programStage && programStage.programStageDataElements) {
+      this.displayInLIst = _.concat(this.displayInLIst, 'eventDate');
+      _.map(
+        programStage.programStageDataElements,
+        (programStageDataElement: any) => {
+          if (
+            programStageDataElement.dataElement &&
+            programStageDataElement.dataElement.id
+          ) {
+            if (programStageDataElement.displayInReports) {
+              this.displayInLIst = _.concat(
+                this.displayInLIst,
+                programStageDataElement.dataElement.id
+              );
+            }
+            let fieldLabel = programStageDataElement.dataElement.displayName;
+            if (
+              dataEntrySettings &&
+              dataEntrySettings.label &&
+              programStageDataElement.dataElement[dataEntrySettings.label]
+            ) {
+              if (
+                isNaN(
+                  programStageDataElement.dataElement[dataEntrySettings.label]
+                )
+              ) {
+                fieldLabel =
+                  programStageDataElement.dataElement[dataEntrySettings.label];
+              }
+            }
+            this.fieldsToDisplay = _.concat(this.fieldsToDisplay, {
+              id: programStageDataElement.dataElement.id,
+              name: fieldLabel
+            });
+          }
+        }
+      );
+    }
+    //for tracker capture listing headers
+    if (programTrackedEntityAttributes) {
+      _.map(
+        programTrackedEntityAttributes,
+        (programTrackedEntityAttribute: any) => {
+          if (programTrackedEntityAttribute.trackedEntityAttribute) {
+            const attribute =
+              programTrackedEntityAttribute.trackedEntityAttribute;
+            let fieldLabel = attribute.name;
+            if (
+              dataEntrySettings &&
+              dataEntrySettings.label &&
+              attribute[dataEntrySettings.label] &&
+              isNaN(attribute[dataEntrySettings.label])
+            ) {
+              fieldLabel = attribute[dataEntrySettings.label];
+            }
+            this.fieldsToDisplay.push({
+              id: attribute.id,
+              name: fieldLabel
+            });
+            if (programTrackedEntityAttribute.displayInList) {
+              this.displayInLIst = _.concat(this.displayInLIst, attribute.id);
+            }
+          }
+        }
+      );
+      if (this.displayInLIst.length === 0 && this.fieldsToDisplay.length > 0) {
+        this.displayInLIst = _.concat(
+          this.displayInLIst,
+          this.fieldsToDisplay[0].id
+        );
+      }
+    }
 
-    console.log('columnsToDisplay : ' + JSON.stringify(columnsToDisplay));
-    console.log('programStage : ' + JSON.stringify(programStage));
-    console.log(
-      'programTrackedEntityAttributes : ' +
-        JSON.stringify(programTrackedEntityAttributes)
-    );
-    console.log('dataEntrySettings : ' + JSON.stringify(dataEntrySettings));
-
-    //columnsToDisplay
-    //programStage.programStageDataElements  programStageDataElement.displayInReports
-    //programTrackedEntityAttributes //programTrackedEntityAttribute.displayInList
+    this.isLoading = false;
   }
 
   ngOnDestroy() {
     this.fieldsToDisplay = null;
     this.selectedItemsModel = null;
+    this.displayInLIst = null;
+    this.isLoading = false;
   }
 
   trackByFn(index, item) {
