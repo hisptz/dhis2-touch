9840865da6b65322bba4e87d1e695d0a099ca6d2
erastolaizer <erastoyusuph@gmail.com>
2018-05-25 00:47:42 +0300

    report filter

--- src/pages/reports/reports.module.ts
+++ src/pages/reports/reports.module.ts
@@ -3,13 +3,14 @@ import { IonicPageModule } from 'ionic-angular';
 import { ReportsPage } from './reports';
 import {SharedModule} from "../../components/shared.module";
 import { TranslateModule} from "@ngx-translate/core";
+import {NgxPaginationModule} from 'ngx-pagination';
 
 @NgModule({
   declarations: [
     ReportsPage,
   ],
   imports: [
-    IonicPageModule.forChild(ReportsPage),SharedModule,
+    IonicPageModule.forChild(ReportsPage),SharedModule, NgxPaginationModule,
     TranslateModule.forChild({})
   ],
 })
