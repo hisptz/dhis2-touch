1990e99a2ff64e7acf2435e759a1ee09ed6d4d79
Joseph Chingalo <profschingalo@gmail.com>
2018-09-04 17:14:54 +0300

    Adding defualt filters for app

--- src/pipes/filter-by-name/filter-by-name.ts
+++ src/pipes/filter-by-name/filter-by-name.ts
@@ -6,18 +6,18 @@ import { Pipe, PipeTransform } from '@angular/core';
  * See https://angular.io/api/core/Pipe for more info on Angular Pipes.
  */
 @Pipe({
-  name: 'filterByName'
+  name: 'filterByName',
 })
 export class FilterByNamePipe implements PipeTransform {
   transform(list: any[], name: any): any {
     const splittedName = name ? name.split(/[\.\-_,; ]/) : [];
     return splittedName.length > 0
       ? list.filter((item: any) =>
-          splittedName.some(
-            (nameString: string) =>
-              item.name.toLowerCase().indexOf(nameString.toLowerCase()) !== -1
-          )
+        splittedName.some(
+          (nameString: string) =>
+            item.name.toLowerCase().indexOf(nameString.toLowerCase()) !== -1
         )
+      )
       : list;
   }
 }
--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
-import { IonicModule } from 'ionic-angular';
+import {IonicModule} from "ionic-angular";
 import { FilterByNamePipe } from './filter-by-name/filter-by-name';
 import { SafePipe } from './safe/safe';
-import { SearchReportListPipe } from './search-report-list/search-report-list';
 @NgModule({
 	declarations: [FilterByNamePipe,
-    SafePipe,
-    SearchReportListPipe],
+    SafePipe],
 	imports: [IonicModule],
 	exports: [FilterByNamePipe,
-    SafePipe,
-    SearchReportListPipe]
+    SafePipe]
 })
 export class PipesModule {}
