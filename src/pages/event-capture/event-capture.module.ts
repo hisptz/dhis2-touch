5ed62890a4adca586f0076ef99b16dee9b397d11
Joseph Chingalo <profschingalo@gmail.com>
2018-12-05 16:06:33 +0300

    feat(Event capture): Adding support to load and handle conflicts for events

--- src/pages/event-capture/event-capture.module.ts
+++ src/pages/event-capture/event-capture.module.ts
@@ -3,11 +3,13 @@ import { IonicPageModule } from 'ionic-angular';
 import { EventCapturePage } from './event-capture';
 import { sharedComponentsModule } from '../../components/sharedComponents.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { EventCaptureComponentsModule } from './components/eventCaptureComponents.module';
 @NgModule({
   declarations: [EventCapturePage],
   imports: [
     IonicPageModule.forChild(EventCapturePage),
     sharedComponentsModule,
+    EventCaptureComponentsModule,
     TranslateModule.forChild({})
   ]
 })
