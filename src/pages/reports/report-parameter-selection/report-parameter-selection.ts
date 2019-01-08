9ea2bb2874f411b35b359a267eb89a7389b19c9e
Joseph Chingalo <profschingalo@gmail.com>
2019-01-08 17:49:00 +0300

    feat(report module): Applying color theming based on user and system settings

--- src/pages/reports/report-parameter-selection/report-parameter-selection.ts
+++ src/pages/reports/report-parameter-selection/report-parameter-selection.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from "@angular/core";
+/*
+ *
+ * Copyright 2015 HISP Tanzania
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @since 2015
+ * @author Joseph Chingalo <profschingalo@gmail.com>
+ *
+ */
+import { Component, OnInit } from '@angular/core';
 import {
   IonicPage,
   ModalController,
   NavController,
   NavParams
-} from "ionic-angular";
-import { UserProvider } from "../../../providers/user/user";
-import { OrganisationUnitsProvider } from "../../../providers/organisation-units/organisation-units";
-import { PeriodSelectionProvider } from "../../../providers/period-selection/period-selection";
-import { StandardReportProvider } from "../../../providers/standard-report/standard-report";
+} from 'ionic-angular';
+import { UserProvider } from '../../../providers/user/user';
+import { OrganisationUnitsProvider } from '../../../providers/organisation-units/organisation-units';
+import { PeriodSelectionProvider } from '../../../providers/period-selection/period-selection';
+import { StandardReportProvider } from '../../../providers/standard-report/standard-report';
+import { Store } from '@ngrx/store';
+import { State, getCurrentUserColorSettings } from '../../../store';
+import { Observable } from 'rxjs';
 
 /**
  * Generated class for the ReportParameterSelectionPage page.
@@ -19,8 +45,8 @@ import { StandardReportProvider } from "../../../providers/standard-report/stand
 
 @IonicPage()
 @Component({
-  selector: "page-report-parameter-selection",
-  templateUrl: "report-parameter-selection.html"
+  selector: 'page-report-parameter-selection',
+  templateUrl: 'report-parameter-selection.html'
 })
 export class ReportParameterSelectionPage implements OnInit {
   reportId: string;
@@ -38,8 +64,10 @@ export class ReportParameterSelectionPage implements OnInit {
   currentPeriodOffset: number = 0;
   isAllReportParameterSet: boolean;
   openFuturePeriods: any;
+  colorSettings$: Observable<any>;
 
   constructor(
+    private store: Store<State>,
     private user: UserProvider,
     private modalCtrl: ModalController,
     private params: NavParams,
@@ -47,20 +75,22 @@ export class ReportParameterSelectionPage implements OnInit {
     private organisationUnitsProvider: OrganisationUnitsProvider,
     private periodSelectionProvider: PeriodSelectionProvider,
     private standardReportProvider: StandardReportProvider
-  ) {}
+  ) {
+    this.colorSettings$ = this.store.select(getCurrentUserColorSettings);
+    this.icons.orgUnit = 'assets/icon/orgUnit.png';
+    this.icons.period = 'assets/icon/period.png';
+    this.icons.report = 'assets/icon/reports.png';
+    this.isAllReportParameterSet = false;
+  }
 
   ngOnInit() {
-    this.icons.orgUnit = "assets/icon/orgUnit.png";
-    this.icons.period = "assets/icon/period.png";
-    this.icons.report = "assets/icon/reports.png";
-    this.isAllReportParameterSet = false;
-    this.openFuturePeriods = this.params.get("openFuturePeriods");
-    this.reportName = this.params.get("name");
-    this.reportId = this.params.get("id");
-    this.reportParams = this.params.get("reportParams");
-    this.reportType = this.params.get("reportType");
+    this.openFuturePeriods = this.params.get('openFuturePeriods');
+    this.reportName = this.params.get('name');
+    this.reportId = this.params.get('id');
+    this.reportParams = this.params.get('reportParams');
+    this.reportType = this.params.get('reportType');
     this.reportPeriodType = this.standardReportProvider.getReportPeriodType(
-      this.params.get("relativePeriods")
+      this.params.get('relativePeriods')
     );
     this.user.getCurrentUser().subscribe(user => {
       this.currentUser = user;
@@ -86,18 +116,18 @@ export class ReportParameterSelectionPage implements OnInit {
       this.selectedOrgUnit = this.organisationUnitsProvider.lastSelectedOrgUnit;
       this.selectedOrganisationUnitLabel = this.selectedOrgUnit.name;
     } else {
-      this.selectedOrganisationUnitLabel = "Touch to select organisation unit";
+      this.selectedOrganisationUnitLabel = 'Touch to select organisation unit';
     }
     if (this.selectedPeriod && this.selectedPeriod.name) {
       this.selectedPeriodLabel = this.selectedPeriod.name;
     } else {
-      this.selectedPeriodLabel = "Touch to select period";
+      this.selectedPeriodLabel = 'Touch to select period';
     }
     this.isAllReportParameterSet = this.isAllReportParameterSelected();
   }
 
   openOrganisationUnitTree() {
-    let modal = this.modalCtrl.create("OrganisationUnitSelectionPage", {});
+    let modal = this.modalCtrl.create('OrganisationUnitSelectionPage', {});
     modal.onDidDismiss((selectedOrgUnit: any) => {
       if (selectedOrgUnit && selectedOrgUnit.id) {
         this.selectedOrgUnit = selectedOrgUnit;
@@ -109,7 +139,7 @@ export class ReportParameterSelectionPage implements OnInit {
 
   openReportPeriodSelection() {
     if (this.selectedOrganisationUnitLabel) {
-      let modal = this.modalCtrl.create("PeriodSelectionPage", {
+      let modal = this.modalCtrl.create('PeriodSelectionPage', {
         periodType: this.reportPeriodType,
         currentPeriodOffset: this.currentPeriodOffset,
         openFuturePeriods: this.openFuturePeriods,
@@ -160,6 +190,6 @@ export class ReportParameterSelectionPage implements OnInit {
           : null,
       organisationUnitChildren: []
     };
-    this.navCtrl.push("ReportViewPage", parameter);
+    this.navCtrl.push('ReportViewPage', parameter);
   }
 }
--- src/pages/reports/report-view/report-view.ts
+++ src/pages/reports/report-view/report-view.ts
@@ -1,3 +1,26 @@
+/*
+ *
+ * Copyright 2015 HISP Tanzania
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @since 2015
+ * @author Joseph Chingalo <profschingalo@gmail.com>
+ *
+ */
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
@@ -7,6 +30,9 @@ import { AppProvider } from '../../../providers/app/app';
 import { StandardReportProvider } from '../../../providers/standard-report/standard-report';
 import { DATABASE_STRUCTURE } from '../../../models/database';
 import { AppTranslationProvider } from '../../../providers/app-translation/app-translation';
+import { Store } from '@ngrx/store';
+import { State, getCurrentUserColorSettings } from '../../../store';
+import { Observable } from 'rxjs';
 
 /**
  * Generated class for the ReportViewPage page.
@@ -34,8 +60,10 @@ export class ReportViewPage implements OnInit {
   currentUser: any;
   reportType: string = '';
   translationMapper: any;
+  colorSettings$: Observable<any>;
 
   constructor(
+    private store: Store<State>,
     private navCtrl: NavController,
     private params: NavParams,
     private user: UserProvider,
@@ -45,11 +73,13 @@ export class ReportViewPage implements OnInit {
     private appProvider: AppProvider,
     private elementRef: ElementRef,
     private appTranslation: AppTranslationProvider
-  ) {}
-
-  ngOnInit() {
+  ) {
+    this.colorSettings$ = this.store.select(getCurrentUserColorSettings);
     this.isLoading = true;
     this.translationMapper = {};
+  }
+
+  ngOnInit() {
     this.appTranslation.getTransalations(this.getValuesToTranslate()).subscribe(
       (data: any) => {
         this.translationMapper = data;
