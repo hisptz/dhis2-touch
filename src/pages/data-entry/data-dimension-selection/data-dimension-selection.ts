86ffc376105b2945cddef2388d0f9b3f0d340146
Joseph Chingalo <profschingalo@gmail.com>
2019-01-09 10:56:29 +0300

    feat(Data entry): Applying color theming on data entry module

--- src/pages/data-entry/data-dimension-selection/data-dimension-selection.ts
+++ src/pages/data-entry/data-dimension-selection/data-dimension-selection.ts
@@ -1,6 +1,32 @@
+/*
+ *
+ * Copyright 2015 HISP Tanzania
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @since 2015
+ * @author Joseph Chingalo <profschingalo@gmail.com>
+ *
+ */
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavParams, ViewController } from 'ionic-angular';
 import { AppTranslationProvider } from '../../../providers/app-translation/app-translation';
+import { Store } from '@ngrx/store';
+import { State, getCurrentUserColorSettings } from '../../../store';
+import { Observable } from 'rxjs';
 
 /**
  * Generated class for the DataDimensionSelectionPage page.
@@ -20,19 +46,23 @@ export class DataDimensionSelectionPage implements OnInit {
   icon: string;
   title: string = 'Data dimension selection';
   translationMapper;
+  colorSettings$: Observable<any>;
 
   constructor(
+    private store: Store<State>,
     private navParams: NavParams,
     private viewCtrl: ViewController,
     private appTranslation: AppTranslationProvider
-  ) {}
+  ) {
+    this.icon = 'assets/icon/form.png';
+    this.translationMapper = {};
+    this.colorSettings$ = this.store.select(getCurrentUserColorSettings);
+  }
 
   ngOnInit() {
-    this.icon = 'assets/icon/form.png';
     this.currentSelection = this.navParams.get('currentSelection');
     this.title = this.navParams.get('title');
     this.categoryOptions = this.navParams.get('categoryOptions');
-    this.translationMapper = {};
     this.appTranslation.getTransalations(this.getValuesToTranslate()).subscribe(
       (data: any) => {
         this.translationMapper = data;
--- src/pages/data-entry/data-entry-form/data-entry-form.ts
+++ src/pages/data-entry/data-entry-form/data-entry-form.ts
@@ -1,3 +1,26 @@
+/*
+ *
+ * Copyright 2015 HISP Tanzania
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @since 2015
+ * @author Joseph Chingalo <profschingalo@gmail.com>
+ *
+ */
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {
   IonicPage,
@@ -14,6 +37,9 @@ import { DataValuesProvider } from '../../../providers/data-values/data-values';
 import { DataSetCompletenessProvider } from '../../../providers/data-set-completeness/data-set-completeness';
 import { AppTranslationProvider } from '../../../providers/app-translation/app-translation';
 import * as _ from 'lodash';
+import { Store } from '@ngrx/store';
+import { State, getCurrentUserColorSettings } from '../../../store';
+import { Observable } from 'rxjs';
 
 /**
  * Generated class for the DataEntryFormPage page.
@@ -54,8 +80,10 @@ export class DataEntryFormPage implements OnInit {
   dataUpdateStatus: { [elementId: string]: string };
   @ViewChild(Content)
   content: Content;
+  colorSettings$: Observable<any>;
 
   constructor(
+    private store: Store<State>,
     private navCtrl: NavController,
     private userProvider: UserProvider,
     private appProvider: AppProvider,
@@ -67,6 +95,7 @@ export class DataEntryFormPage implements OnInit {
     private navParams: NavParams,
     private appTranslation: AppTranslationProvider
   ) {
+    this.colorSettings$ = this.store.select(getCurrentUserColorSettings);
     this.dataEntryFormDesign = '';
     this.entryFormType = 'SECTION';
     this.icons['menu'] = 'assets/icon/menu.png';
