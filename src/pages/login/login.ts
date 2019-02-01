5509f6bdab204dfa0b401ea7fef0dec4a9d24a36
Joseph Chingalo <profschingalo@gmail.com>
2019-02-01 10:09:31 +0300

    fix(Mutliple instance): Fixing loading metadata on multiple instances

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -123,7 +123,7 @@ export class LoginPage implements OnInit, OnDestroy {
 
   ngOnInit() {
     const defaultCurrentUser: CurrentUser = {
-      serverUrl: 'play.dhis2.org/2.28', //'dhis.hisptz.org/eds', // 'ssudanhis.org', //'play.dhis2.org/2.28',
+      serverUrl: 'https://dhis.hisptz.org/edsmobile', //'dhis.hisptz.org/eds', // 'ssudanhis.org', //'play.dhis2.org/2.28',
       username: 'admin', // 'boma',
       password: 'district', // 'Boma_2018',
       currentLanguage: 'en',
@@ -336,6 +336,7 @@ export class LoginPage implements OnInit, OnDestroy {
   }
 
   startLoginProcess() {
+    this.currentUser = { ...this.currentUser, isPasswordEncode: false };
     this.overAllLoginMessage = this.currentUser.serverUrl;
     this.isLoginProcessActive = true;
     this.backgroundMode.enable();
