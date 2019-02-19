0d119ed4f3a05eea62f3f0e6ed28d9c1218a7eeb
Joseph Chingalo <profschingalo@gmail.com>
2019-02-19 18:31:51 +0300

    refactor(Data entry): Reset back
    Reset back to most table commit f083362a9d0db8b49cf85d4c60332b09e830349d

--- src/pages/data-entry/helpers/data-entry.helper.ts
+++ src/pages/data-entry/helpers/data-entry.helper.ts
@@ -33,18 +33,10 @@ function getSanitizedValue(value, type) {
 }
 
 function convertToBoolean(stringValue) {
-  return stringValue === 'true'
-    ? Boolean(true)
-    : 'false'
-    ? Boolean(false)
-    : stringValue;
+  return stringValue === 'true' ? Boolean(true) : stringValue;
 }
 
 function getSelectInput(id, value, options) {
-  console.log('id'+id);
-  if (id == 'f0VAv7CxDZW-vlqvgwNklG7-val') {
-    console.log('options' + JSON.stringify(options));
-  }
   const selectElement = document.createElement('select');
   selectElement.setAttribute('id', id);
   selectElement.setAttribute('class', 'entryselect');
@@ -85,15 +77,17 @@ function getRadioInputs(id, savedValue) {
   if (savedValue == 'true') {
     const yesInput = document.createElement('input');
     yesInput.setAttribute('type', 'radio');
+    yesInput.setAttribute('id', id);
     yesInput.setAttribute('name', id);
-    yesInput.setAttribute('class', 'entryfield');
+    yesInput.setAttribute('class', 'entryfield-radio');
     yesInput.checked = true;
     yesInput.value = 'true';
 
     const noInput = document.createElement('input');
     noInput.setAttribute('type', 'radio');
+    noInput.setAttribute('id', id);
     noInput.setAttribute('name', id);
-    noInput.setAttribute('class', 'entryfield');
+    noInput.setAttribute('class', 'entryfield-radio');
     noInput.value = 'false';
 
     radioContainer.appendChild(yesInput);
@@ -105,13 +99,15 @@ function getRadioInputs(id, savedValue) {
     const yesInput = document.createElement('input');
     yesInput.setAttribute('type', 'radio');
     yesInput.setAttribute('name', id);
-    yesInput.setAttribute('class', 'entryfield');
+    yesInput.setAttribute('id', id);
+    yesInput.setAttribute('class', 'entryfield-radio');
     yesInput.value = 'true';
 
     const noInput = document.createElement('input');
     noInput.setAttribute('type', 'radio');
     noInput.setAttribute('name', id);
-    noInput.setAttribute('class', 'entryfield');
+    noInput.setAttribute('id', id);
+    noInput.setAttribute('class', 'entryfield-radio');
     noInput.checked = true;
     noInput.value = 'false';
 
@@ -123,14 +119,16 @@ function getRadioInputs(id, savedValue) {
   } else {
     const yesInput = document.createElement('input');
     yesInput.setAttribute('type', 'radio');
+    yesInput.setAttribute('id', id);
     yesInput.setAttribute('name', id);
-    yesInput.setAttribute('class', 'entryfield');
+    yesInput.setAttribute('class', 'entryfield-radio');
     yesInput.value = 'true';
 
     const noInput = document.createElement('input');
     noInput.setAttribute('type', 'radio');
+    noInput.setAttribute('id', id);
     noInput.setAttribute('name', id);
-    noInput.setAttribute('class', 'entryfield');
+    noInput.setAttribute('class', 'entryfield-radio');
     noInput.value = 'false';
 
     radioContainer.appendChild(yesInput);
@@ -152,18 +150,183 @@ export function updateFormFieldColor(elementId, statusColor) {
 
 function getDataValue(data, id) {
   var dataObject = data[id];
-  return dataObject ? dataObject.value : null;
+  return dataObject ? dataObject.value : '';
 }
 
 export function onFormReady(
   formType,
   dataElements,
+  programIndicators,
+  indicators,
   dataValues,
   entryFormStatusColors,
   scriptsContentsArray,
   formReady
 ) {
-  formReady(formType, entryFormStatusColors, scriptsContentsArray);
+  // Find input items and set required properties to them
+  const dataElementObjects = _.keyBy(dataElements, 'id');
+  const inputElements: any = document.getElementsByTagName('INPUT');
+  const elementsWithOptionSet = {};
+  const elementsWithTextArea = {};
+  const elementsWithRadioInput = {};
+  _.each(inputElements, (inputElement: any) => {
+    if (inputElement) {
+      //empty value set on design inputs
+      if (inputElement && inputElement.hasAttribute('value')) {
+        inputElement.setAttribute('value', '');
+      }
+      // Get attribute from the element
+      const elementId = inputElement.getAttribute('id')
+        ? inputElement.getAttribute('id')
+        : inputElement.getAttribute('attributeid');
+
+      // Get splitted ID to get data element and category combo ids
+      const splitedId =
+        formType === 'aggregate' || formType === 'event'
+          ? elementId
+            ? elementId.split('-')
+            : []
+          : [];
+
+      const dataElementId = formType === 'event' ? splitedId[1] : splitedId[0];
+      const optionComboId =
+        formType === 'event'
+          ? 'dataElement'
+          : formType === 'tracker'
+          ? 'trackedEntityAttribute'
+          : splitedId[1];
+
+      // // Get data element details
+
+      const dataElementDetails = dataElementObjects[dataElementId]
+        ? dataElementObjects[dataElementId]
+        : {};
+
+      // // Get dataElement type
+      const dataElementType = dataElementDetails
+        ? dataElementDetails.valueType
+        : null;
+
+      // // Get element value
+      const dataElementValue = getSanitizedValue(
+        getDataValue(dataValues, dataElementId + '-' + optionComboId),
+        dataElementType
+      );
+      // // Update DOM based on data element type
+      if (dataElementType) {
+        if (dataElementDetails.optionSet) {
+          const selectInput = getSelectInput(
+            elementId,
+            dataElementValue,
+            dataElementDetails.optionSet.options
+          );
+          elementsWithOptionSet[elementId] = selectInput;
+        } else {
+          if (dataElementType === 'TRUE_ONLY') {
+            inputElement.setAttribute('type', 'checkbox');
+            inputElement.setAttribute('class', 'entrytrueonly');
+            inputElement.checked = dataElementValue;
+          } else if (dataElementType === 'LONG_TEXT') {
+            elementsWithRadioInput[elementId] = {
+              textAreaInput: inputElement.replaceWith(
+                getTextArea(elementId, dataElementValue)
+              ),
+              dataElementValue
+            };
+          } else if (dataElementType === 'DATE') {
+            inputElement.setAttribute('type', 'date');
+            inputElement.setAttribute('class', 'entryfield');
+            inputElement.value = dataElementValue;
+          } else if (dataElementType === 'BOOLEAN') {
+            elementsWithRadioInput[elementId] = getRadioInputs(
+              elementId,
+              dataElementValue
+            );
+          } else if (
+            dataElementType === 'PERCENTAGE' ||
+            dataElementType === 'NUMBER' ||
+            dataElementType.indexOf('INTEGER') > -1
+          ) {
+            inputElement.setAttribute('type', 'number');
+            inputElement.setAttribute('class', 'entryfield');
+            if (dataElementType === 'INTEGER_POSITIVE') {
+              inputElement.setAttribute('min', 1);
+            } else if (dataElementType === 'INTEGER_NEGATIVE') {
+              inputElement.setAttribute('max', -1);
+            } else if (dataElementType === 'INTEGER_ZERO_OR_POSITIVE') {
+              inputElement.setAttribute('min', 0);
+            } else if (dataElementType === 'PERCENTAGE') {
+              inputElement.setAttribute('min', 0);
+              inputElement.setAttribute('max', 100);
+            }
+            inputElement.value = dataElementValue;
+          } else {
+            inputElement.setAttribute('class', 'entryfield');
+            inputElement.value = dataElementValue;
+          }
+        }
+      } else {
+        // TODO Find ways to deal with input that
+        if (
+          inputElement &&
+          inputElement.hasAttribute('name') &&
+          inputElement.getAttribute('name') === 'indicator'
+        ) {
+          inputElement.setAttribute('value', '0');
+          inputElement.setAttribute('class', 'entryfield');
+          inputElement.setAttribute('readonly', 'readonly');
+          inputElement.setAttribute('disabled', 'disabled');
+        }
+      }
+    }
+  });
+
+  // update option sets
+  for (let elementId of Object.keys(elementsWithOptionSet)) {
+    try {
+      const inputElement: any = document.getElementById(elementId);
+      const selectInput = elementsWithOptionSet[elementId];
+      inputElement.replaceWith(selectInput);
+    } catch (error) {
+      console.log(JSON.stringify({ type: 'Select input', error }));
+    }
+  }
+
+  // update option sets
+  for (let elementId of Object.keys(elementsWithRadioInput)) {
+    try {
+      const inputElement: any = document.getElementById(elementId);
+      const redioInput = elementsWithRadioInput[elementId];
+      inputElement.replaceWith(redioInput);
+    } catch (error) {
+      console.log(JSON.stringify({ type: 'Radio input', error }));
+    }
+  }
+
+  // update text area
+  for (let elementId of Object.keys(elementsWithTextArea)) {
+    try {
+      const inputElement: any = document.getElementById(elementId);
+      const textAreaInputObject = elementsWithTextArea[elementId];
+      const { textAreaInput, dataElementValue } = textAreaInputObject;
+      inputElement.replaceWith(textAreaInput);
+      inputElement.value = dataElementValue;
+    } catch (error) {
+      console.log(JSON.stringify({ type: 'Text area input', error }));
+    }
+  }
+  if (formType === 'event') {
+    evaluateCustomFomProgramIndicators(programIndicators);
+  } else if (formType === 'aggregate') {
+    evaluateCustomFomAggregateIndicators(indicators);
+  }
+  formReady(
+    formType,
+    entryFormStatusColors,
+    programIndicators,
+    indicators,
+    scriptsContentsArray
+  );
 }
 
 export function onDataValueChange(
@@ -201,4 +364,76 @@ export function onDataValueChange(
     }
   });
   document.body.dispatchEvent(dataValueEvent);
+}
+
+export function evaluateCustomFomProgramIndicators(programIndicators: any[]) {
+  for (let programIndicator of programIndicators) {
+    const { id, expression, filter, name } = programIndicator;
+    if (filter) {
+      console.log(JSON.stringify({ filter }));
+    }
+    const indicatorValue = getProgramIndicatorValueFromExpression(expression);
+    const element: any = document.getElementById(`indicator${id}`);
+    if (element) {
+      element.value = indicatorValue;
+    }
+  }
+}
+
+export function evaluateCustomFomAggregateIndicators(indicators: any[]) {
+  console.log(JSON.stringify(indicators));
+}
+
+function getProgramIndicatorValueFromExpression(expression: string) {
+  let indicatorValue = '0';
+  const indictorUidValue = {};
+  const uids = getUidsFromExpression(expression);
+  for (const uid of uids) {
+    const elementId = uid.split('.').join('-');
+    const element: any = document.getElementById(`${elementId}-val`);
+    const value = element && element.value ? element.value : '0';
+    indictorUidValue[uid] = value;
+  }
+  indicatorValue = getEvaluatedIndicatorValueFromExpression(
+    expression,
+    indictorUidValue
+  );
+  return indicatorValue;
+}
+
+function getUidsFromExpression(expression) {
+  let uids = [];
+  const matchRegrex = /(\{.*?\})/gi;
+  expression.match(matchRegrex).forEach(function(value) {
+    uids = uids.concat(
+      value
+        .replace('{', ':separator:')
+        .replace('}', ':separator:')
+        .split(':separator:')
+        .filter(content => content.length > 0)
+    );
+  });
+  return uids;
+}
+function getEvaluatedIndicatorValueFromExpression(
+  expression,
+  indicatorIdToValueObject
+) {
+  let evaluatedValue = 0;
+  const formulaPattern = /#\{.+?\}/g;
+  const matcher = expression.match(formulaPattern);
+  matcher.map(function(match) {
+    var operand = match.replace(/[#\{\}]/g, '');
+    const value =
+      indicatorIdToValueObject && indicatorIdToValueObject[operand]
+        ? indicatorIdToValueObject[operand]
+        : 0;
+    expression = expression.replace(match, value);
+  });
+  try {
+    if (!isNaN(eval(expression))) {
+      evaluatedValue = eval(expression);
+    }
+  } catch (e) {}
+  return evaluatedValue.toFixed(1);
 }
