3746dcbdb78965e93a3f3280323afb8ab0a8f2ee
Joseph Chingalo <profschingalo@gmail.com>
2019-04-04 12:11:39 +0300

    chore: Update testing server

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -124,7 +124,7 @@ export class LoginPage implements OnInit, OnDestroy {
 
   ngOnInit() {
     const defaultCurrentUser: CurrentUser = {
-      serverUrl: 'play.dhis2.org/2.30', //'https://dhis.hisptz.org/edsmobile', //'dhis.hisptz.org/eds', // 'ssudanhis.org', //'play.dhis2.org/2.28',
+      serverUrl: 'play.dhis2.org/2.28', //'https://dhis.hisptz.org/edsmobile', //'dhis.hisptz.org/eds', // 'ssudanhis.org', //'play.dhis2.org/2.28',
       username: 'admin', // 'boma',
       password: 'district', // 'Boma_2018',
       currentLanguage: 'en',
