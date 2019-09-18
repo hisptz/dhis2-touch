3b70b45d9fbbe55e75b317fc12163ff59ae343fc
Joseph Chingalo <profschingalo@gmail.com>
2019-09-18 14:32:58 +0300

    feat: SUpport of selection as well as persintance on event capture

--- src/app/tabs/apps/event-capture/components/program-based-paramater-selection/program-based-paramater-selection.component.ts
+++ src/app/tabs/apps/event-capture/components/program-based-paramater-selection/program-based-paramater-selection.component.ts
@@ -23,9 +23,13 @@
  */
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { Option, CurrentEntrySelection } from 'src/models';
+import * as _ from 'lodash';
+import { Option, CurrentEntrySelection, Program } from 'src/models';
+import { DEFAULT_CURRENT_ENTRY_SELECTION } from 'src/constants';
 import { OrganisationUnitSelectionPage } from 'src/app/modals/organisation-unit-selection/organisation-unit-selection.page';
 import { OptionSetSelectionPage } from 'src/app/modals/option-set-selection/option-set-selection.page';
+import { ProgramSelectionService } from 'src/app/services/program-selection.service';
+import { ToasterMessagesService } from 'src/app/services/toaster-messages.service';
 
 @Component({
   selector: 'app-program-based-paramater-selection',
@@ -34,6 +38,10 @@ import { OptionSetSelectionPage } from 'src/app/modals/option-set-selection/opti
 })
 export class ProgramBasedParamaterSelectionComponent implements OnInit {
   @Input() currentEntrySelection: CurrentEntrySelection;
+  @Input() allowMultipleOuSelection: boolean;
+  @Input() authorities: string[];
+  @Input() programIdsByUserRoles: string[];
+  @Input() programType: string;
 
   @Output() currentSelectionChange = new EventEmitter();
 
@@ -44,48 +52,318 @@ export class ProgramBasedParamaterSelectionComponent implements OnInit {
   isFormReady: boolean;
   isProgramDimensionApplicable: boolean;
   programOptions: Option[];
+  programCategoryCombo: any;
+  selectedDataDimension: any[];
+  selectedProgram: any;
 
-  constructor(private modalController: ModalController) {
+  constructor(
+    private modalController: ModalController,
+    private programSelectionService: ProgramSelectionService,
+    private toasterMessagesService: ToasterMessagesService
+  ) {
     this.organisationUnitIcon = `assets/icon/orgUnit.png`;
     this.programIcon = 'assets/icon/program.png';
     this.isFormReady = false;
     this.isProgramDimensionApplicable = false;
+    this.programCategoryCombo = {};
+    this.selectedProgram = null;
   }
 
   ngOnInit() {
-    this.organisationUnitLabel = 'Touch to select organisation unit';
-    this.programLabel = 'Touch to select program';
+    this.allowMultipleOuSelection = this.allowMultipleOuSelection || false;
+    this.currentEntrySelection =
+      this.currentEntrySelection || DEFAULT_CURRENT_ENTRY_SELECTION;
+    this.selectedDataDimension = this.currentEntrySelection.selectedDataDimension;
+    this.setProgramParameterSelections();
+  }
+
+  async setProgramParameterSelections() {
+    if (
+      this.currentEntrySelection &&
+      this.currentEntrySelection.selectedOrganisationUnit.name &&
+      this.currentEntrySelection.selectedOrganisationUnit.name !== ''
+    ) {
+      await this.discoveringProgramList();
+    } else {
+      await this.updateProgramParameterSelections();
+    }
+  }
+
+  async updateProgramParameterSelections() {
+    await this.setOrganisationUnitLabel();
+    await this.setProgramLabel();
+    await this.setProgramCategoryLabel();
+    const isFormReady = await this.isAllParameterSelected();
+    setTimeout(() => {
+      this.currentSelectionChange.emit({
+        isFormReady,
+        currentEntrySelection: isFormReady ? this.currentEntrySelection : null
+      });
+    }, 100);
+  }
+
+  async setOrganisationUnitLabel() {
+    if (
+      this.currentEntrySelection &&
+      this.currentEntrySelection.selectedOrganisationUnit.name &&
+      this.currentEntrySelection.selectedOrganisationUnit.name !== ''
+    ) {
+      this.organisationUnitLabel = this.currentEntrySelection.selectedOrganisationUnit.name;
+    } else {
+      this.organisationUnitLabel = 'Touch to select organisation unit';
+    }
+  }
+
+  async setProgramLabel() {
+    if (
+      this.currentEntrySelection &&
+      this.currentEntrySelection.selectedProgram.name &&
+      this.currentEntrySelection.selectedProgram.name !== ''
+    ) {
+      this.programLabel = this.currentEntrySelection.selectedProgram.name;
+    } else {
+      this.programLabel = 'Touch to select program';
+    }
+  }
+
+  async setProgramCategoryLabel() {
+    const dataDimension = await this.getDataDimension();
+    this.currentEntrySelection = {
+      ...this.currentEntrySelection,
+      selectedDataDimension: this.selectedDataDimension,
+      dataDimension
+    };
+  }
+
+  async isAllParameterSelected() {
+    let isAllParameterSelected = false;
+    const isEmptySelectedDimension = _.filter(
+      this.selectedDataDimension,
+      (item: any) => {
+        return _.isEmpty(item);
+      }
+    );
+    if (
+      this.selectedProgram &&
+      this.selectedProgram.categoryCombo &&
+      this.selectedProgram.categoryCombo.name
+    ) {
+      const { categoryCombo } = this.selectedProgram;
+      const { name } = categoryCombo;
+      isAllParameterSelected =
+        name === 'default'
+          ? true
+          : isEmptySelectedDimension.length === 0 &&
+            this.selectedDataDimension.length > 0
+          ? true
+          : isAllParameterSelected;
+    }
+    return isAllParameterSelected;
+  }
+
+  async getDataDimension() {
+    let attributeCos = '';
+    let attributeCc = '';
+    if (this.selectedProgram && this.selectedProgram.categoryCombo) {
+      const { categoryCombo } = this.selectedProgram;
+      attributeCc = categoryCombo && categoryCombo.id ? categoryCombo.id : '';
+      if (this.selectedDataDimension.length > 0) {
+        attributeCos = _.join(
+          _.map(
+            _.filter(this.selectedDataDimension, (item: any) => {
+              return !_.isEmpty(item);
+            }),
+            (item: any) => item.id
+          ),
+          ';'
+        );
+      }
+    }
+    return { attributeCos, attributeCc };
   }
 
   async openOrganisationUnitModal() {
+    const organisationUnitId = this.currentEntrySelection
+      .selectedOrganisationUnit.id;
     const modal = await this.modalController.create({
       component: OrganisationUnitSelectionPage,
       componentProps: {
-        allowMultipleSelection: false,
-        selectedOrganisayionUnitIds: ['V5XvX1wr1kF']
-      },
-      cssClass: 'inset-modal'
+        allowMultipleSelection: this.allowMultipleOuSelection,
+        selectedOrganisayionUnitIds: [organisationUnitId]
+      }
     });
     modal.present();
     const response = await modal.onDidDismiss();
     if (response && response.data) {
-      console.log({ data: response.data });
+      const { id, name } = response.data;
+      this.setCurrentOrganisationUnit(id, name);
+      this.updateProgramParameterSelections();
+      await this.discoveringProgramList();
     }
   }
 
-  async OpenProgramListSelectionModal() {
+  async openProgramListSelectionModal() {
+    const programId = this.currentEntrySelection.selectedProgram.id;
     const modal = await this.modalController.create({
       component: OptionSetSelectionPage,
       componentProps: {
-        selectedValue: '',
+        selectedValue: programId,
         options: this.programOptions
-      },
-      cssClass: 'inset-modal'
+      }
     });
     modal.present();
     const response = await modal.onDidDismiss();
     if (response && response.data) {
-      console.log({ data: response.data });
+      const { id, name } = response.data;
+      this.setCurrentProgram(id, name);
+      this.updateProgramParameterSelections();
+      await this.discoveringProgramList();
     }
+  }
+
+  async openDataDimensionSelection(category: any) {
+    const currentIndex = _.indexOf(
+      this.programCategoryCombo.categories,
+      category
+    );
+    const selectedValue =
+      currentIndex > -1 &&
+      this.selectedDataDimension &&
+      this.selectedDataDimension[currentIndex]
+        ? this.selectedDataDimension[currentIndex].id
+        : '';
+    const modal = await this.modalController.create({
+      component: OptionSetSelectionPage,
+      componentProps: {
+        selectedValue,
+        options: _.map(category.categoryOptions, (categoryOption: any) => {
+          const { id } = categoryOption;
+          return { ...categoryOption, code: id };
+        })
+      }
+    });
+    modal.present();
+    const response = await modal.onDidDismiss();
+    if (response && response.data) {
+      const { id, name } = response.data;
+      const selectedDataDimension = { id, name };
+      this.setSelectedCategoryOption(selectedDataDimension, category);
+    }
+  }
+
+  setCurrentOrganisationUnit(id: string, name: string) {
+    this.currentEntrySelection.selectedOrganisationUnit = {
+      ...this.currentEntrySelection.selectedOrganisationUnit,
+      id,
+      name
+    };
+  }
+
+  setSelectedCategoryOption(selectedDataDimension: any, category: any) {
+    const currentIndex = _.indexOf(
+      this.programCategoryCombo.categories,
+      category
+    );
+    this.selectedDataDimension[currentIndex] = selectedDataDimension;
+    this.updateProgramParameterSelections();
+  }
+
+  setCurrentProgram(id: string, name: string) {
+    this.currentEntrySelection.selectedProgram = {
+      ...this.currentEntrySelection.selectedProgram,
+      id,
+      name
+    };
+  }
+
+  async discoveringProgramList() {
+    try {
+      const organisationUnitId = this.currentEntrySelection
+        .selectedOrganisationUnit.id;
+      const programId = this.currentEntrySelection.selectedProgram.id;
+      const programs: any[] = await this.programSelectionService.getProgramListBySelectedOrganisationUnitAndRoles(
+        organisationUnitId,
+        this.programType,
+        this.programIdsByUserRoles,
+        this.authorities
+      );
+      this.resetCurrentProgramAndProgramList(programs, programId);
+      if (this.currentEntrySelection.selectedProgram.id !== '') {
+        this.selectedProgram = _.find(programs, (program: Program) => {
+          return program.id === this.currentEntrySelection.selectedProgram.id;
+        });
+        if (this.selectedProgram && this.selectedProgram.categoryCombo) {
+          await this.setProgramCategoryCombos(
+            this.selectedProgram,
+            organisationUnitId
+          );
+        }
+      }
+      this.programOptions = _.map(programs, (program: any) => {
+        const { id, code, name } = program;
+        return { id, name, code };
+      });
+    } catch (error) {
+      const message = `Error : ${JSON.stringify(error)}`;
+      this.toasterMessagesService.showToasterMessage(message);
+    } finally {
+      setTimeout(() => {
+        this.updateProgramParameterSelections();
+      }, 100);
+    }
+  }
+
+  resetCurrentProgramAndProgramList(programs: any[], programId: string) {
+    const programObj: any = _.find(programs, (program: Program) => {
+      return program.id === programId;
+    });
+    if (programObj && programObj.id) {
+      const { id, name } = programObj;
+      this.setCurrentProgram(id, name);
+    } else if (programs && programs.length > 0) {
+      const program = programs[0];
+      const { id, name } = program;
+      this.setCurrentProgram(id, name);
+    } else {
+      this.setCurrentProgram('', '');
+    }
+  }
+
+  async setProgramCategoryCombos(
+    selectedProgram: Program,
+    organisationUnitId: string
+  ) {
+    this.isProgramDimensionApplicable = false;
+    const { categoryCombo } = selectedProgram;
+    const { id, name, categories } = categoryCombo;
+    this.programCategoryCombo = {};
+    if (name !== 'default') {
+      this.programCategoryCombo['id'] = id;
+      this.programCategoryCombo['name'] = name;
+      const categoriesResponse = await this.programSelectionService.getProgramCategoryComboCategories(
+        organisationUnitId,
+        categories
+      );
+      this.programCategoryCombo['categories'] = categoriesResponse;
+      this.isProgramDimensionApplicable = true;
+      for (const category of categoriesResponse) {
+        if (
+          category &&
+          category.categoryOptions &&
+          category.categoryOptions.length === 0
+        ) {
+          this.isProgramDimensionApplicable = false;
+        }
+      }
+    }
+    this.selectedDataDimension =
+      this.currentEntrySelection.dataDimension.attributeCc === id
+        ? []
+        : this.currentEntrySelection.selectedDataDimension;
+    this.updateProgramParameterSelections();
+  }
+
+  trackByFn(index: any, item: any) {
+    return item && item.id ? item.id : index;
   }
 }
