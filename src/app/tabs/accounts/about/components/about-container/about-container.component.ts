7bc9f0aa53146774e15d97db03ebac458914947c
glad-son <gsonly.imo@gmail.com>
2019-10-01 11:46:22 +0300

    improve ui

--- src/app/tabs/accounts/about/components/about-container/about-container.component.ts
+++ src/app/tabs/accounts/about/components/about-container/about-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { AppAboutContent } from "src/models";
+import * as _ from "lodash";
 
 @Component({
   selector: "app-about-container",
@@ -7,7 +9,29 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class AboutContainerComponent implements OnInit {
   @Input() systemInfoContent: any[];
+  @Input() aboutContents: AppAboutContent[];
+  onToggle: boolean;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.aboutContents && this.aboutContents.length > 0) {
+      const aboutContent = this.aboutContents[0];
+      this.toggleContent(aboutContent);
+    }
+  }
+
+  toggleContent(aboutContent: AppAboutContent) {
+    const { id, isOpened } = aboutContent;
+    const aboutContents = _.map(this.aboutContents, _.cloneDeep);
+    this.aboutContents = _.map(
+      aboutContents,
+      (aboutContentObj: AppAboutContent) => {
+        return {
+          ...aboutContentObj,
+          isOpened: id === aboutContentObj.id ? !isOpened : false
+        };
+      }
+    );
+  }
 }
