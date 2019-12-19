6602616e8fc73f52fc27d9dcaf8cc8ccff8120a3
Joseph Chingalo <profschingalo@gmail.com>
2019-12-19 14:23:27 +0300

    chore(app): set status color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,7 +44,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
       this.splashScreen.hide();
     });
   }
